1e3d744ff1ac315b71f449a065a4bf3feb88b22f
cklewar@t-online.de <cklewar@t-online.de>
2022-08-02 12:59:22 +0200

    blindfold skeleton

---
+++ f5xc/blindfold/variables.tf
@@ -0,0 +1,35 @@
+variable "f5xc_api_url" {
+  type = string
+}
+
+variable "f5xc_api_cert" {
+  type    = string
+  default = ""
+}
+
+variable "f5xc_api_key" {
+  type    = string
+  default = ""
+}
+
+variable "f5xc_api_ca_cert" {
+  type    = string
+  default = ""
+}
+
+variable "f5xc_api_p12_file" {
+  type = string
+}
+
+variable "f5xc_api_token" {
+  type    = string
+  default = ""
+}
+
+variable "f5xc_tenant" {
+  type = string
+}
+
+variable "f5xc_namespace" {
+  type = string
+}

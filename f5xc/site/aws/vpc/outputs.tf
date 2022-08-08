4e811501bbd160a0eaa22e21a22b38627164cb60
cklewar@t-online.de <cklewar@t-online.de>
2022-08-08 10:25:37 +0200

    fix output values

--- f5xc/site/aws/vpc/outputs.tf
+++ f5xc/site/aws/vpc/outputs.tf
@@ -1,7 +1,7 @@
 output "f5xc_aws_vpc_id" {
-  value = volterra_aws_vpc_site.aws_vpc_site.id
+  value = volterra_aws_vpc_site.vpc.id
 }
 
 output "f5xc_aws_vpc_name" {
-  value = volterra_aws_vpc_site.aws_vpc_site.name
+  value = volterra_aws_vpc_site.vpc.name
 }

ddff9746d82b2b2a937ecf47cc3a2fc78821c8cf
cklewar@t-online.de <cklewar@t-online.de>
2022-08-08 21:47:12 +0200

    update aws ec2 module

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -1,7 +1,6 @@
 resource "aws_key_pair" "aws-key" {
   key_name   = format("%s-key", var.aws_ec2_instance_name)
-  # public_key = file(format("%s/keys/%s", path.module, var.ssh_public_key_file))
-  public_key = "modules/aws/ec2/keys/key.pub"
+  public_key = file(format("%s/keys/%s", path.module, var.ssh_public_key_file))
 }
 
 resource "aws_security_group" "public" {

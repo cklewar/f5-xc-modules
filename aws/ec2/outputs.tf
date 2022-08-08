7a58fd419e79bd42005cbbc2327a9025dbfa82b6
cklewar@t-online.de <cklewar@t-online.de>
2022-08-08 22:40:24 +0200

    update aws ec2 module

--- aws/ec2/outputs.tf
+++ aws/ec2/outputs.tf
@@ -21,3 +21,14 @@ output "aws_ec2_instance_public_ip" {
 output "aws_ec2_instance_private_ip" {
   value = aws_network_interface.private.private_ip
 }
+
+output "aws_ec2_instance" {
+  value = {
+    "script_source"      = abspath(format("../modules/ec2/_out/%s", format("%s.sh", var.aws_ec2_instance_script_file)))
+    "script_destination" = format("/tmp/%s", format("%s.sh", var.aws_ec2_instance_script_file))
+    "userdata_source"    = abspath(format("../modules/ec2/userdata/%s", var.aws_ec2_instance_script_file))
+    "public_dns"         = aws_instance.instance.public_dns
+    "public_ip"          = aws_eip.eip.public_ip
+    "private_ip"         = aws_network_interface.private.private_ip
+  }
+}

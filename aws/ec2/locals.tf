af01ce9c0f0fbc812129639a6202a773e2da0b87
cklewar@t-online.de <cklewar@t-online.de>
2022-08-08 21:23:42 +0200

    update aws ec2 module

--- aws/ec2/locals.tf
+++ aws/ec2/locals.tf
@@ -1,4 +1,5 @@
 locals {
-  userdata_content = templatefile(format("%s/templates/%s", path.module, var.aws_ec2_instance_script_template), var.aws_ec2_instance_data.template_data)
+  # userdata_content = templatefile(format("%s/templates/%s", path.module, var.aws_ec2_instance_script_template), var.aws_ec2_instance_data.template_data)
+  userdata_content = templatefile(format("%s/templates/%s", path.module, var.aws_ec2_instance_script_template), var.aws_ec2_instance_data.userdata)
 }
 

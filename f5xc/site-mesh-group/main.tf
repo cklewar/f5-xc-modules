7ae2661ec19a9b544b55ca32f27d9c9b813b2c17
cklewar@t-online.de <cklewar@t-online.de>
2022-08-08 23:38:05 +0200

    update virtual site module

--- f5xc/site-mesh-group/main.tf
+++ f5xc/site-mesh-group/main.tf
@@ -6,7 +6,7 @@ resource "volterra_site_mesh_group" "site_mesh_group" {
   description = var.f5xc_site_mesh_group_description
 
   dynamic "hub" {
-    for_each = var.f5xc_site_2_site_connection_type != var.f5xc_site_2_site_connection_type_hub && var.f5xc_site_site_hub_name != "" ? 1 : 0
+    for_each = var.f5xc_site_2_site_connection_type != var.f5xc_site_2_site_connection_type_hub && var.f5xc_site_site_hub_name != "" ? [1] : [0]
     content {
       name      = var.f5xc_site_site_hub_name
       namespace = var.f5xc_namespace
@@ -15,7 +15,7 @@ resource "volterra_site_mesh_group" "site_mesh_group" {
   }
 
   dynamic "virtual_site" {
-    for_each = var.f5xc_virtual_site_name != "" ? 1 : 0
+    for_each = var.f5xc_virtual_site_name != "" ? [1] : [0]
     content {
       name      = var.f5xc_virtual_site_name
       namespace = var.f5xc_namespace

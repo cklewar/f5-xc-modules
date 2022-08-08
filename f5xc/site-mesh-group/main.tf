024b6a2e646d6f9f65ae22e666d0f44b50c26e10
cklewar@t-online.de <cklewar@t-online.de>
2022-08-08 23:35:46 +0200

    update virtual site module

--- f5xc/site-mesh-group/main.tf
+++ f5xc/site-mesh-group/main.tf
@@ -2,7 +2,7 @@ resource "volterra_site_mesh_group" "site_mesh_group" {
   name        = var.f5xc_site_mesh_group_name
   namespace   = var.f5xc_namespace
   tunnel_type = [var.f5xc_tunnel_type]
-  type        = [var.f5xc_site_2_site_connection_type]
+  type        = var.f5xc_site_2_site_connection_type
   description = var.f5xc_site_mesh_group_description
 
   dynamic "hub" {

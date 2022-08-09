1cec981b9c74e8289d63d3573b0ac8b2767d06af
cklewar@t-online.de <cklewar@t-online.de>
2022-08-09 02:29:27 +0200

    update aws vpc module

--- f5xc/site/aws/vpc/outputs.tf
+++ f5xc/site/aws/vpc/outputs.tf
@@ -5,3 +5,12 @@ output "f5xc_aws_vpc_id" {
 output "f5xc_aws_vpc_name" {
   value = volterra_aws_vpc_site.vpc.name
 }
+
+output "aws_vpc" {
+  value = {
+    "name"          = volterra_aws_vpc_site.vpc.name
+    "id"            = volterra_aws_vpc_site.vpc.id
+    "region"        = volterra_aws_vpc_site.vpc.aws_region
+    "instance_type" = volterra_aws_vpc_site.vpc.instance_type
+  }
+}

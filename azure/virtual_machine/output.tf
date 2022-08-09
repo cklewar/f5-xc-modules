b84eff423b92638c82017197b4a4250868bdc480
cklewar@t-online.de <cklewar@t-online.de>
2022-08-09 22:31:30 +0200

    update azure module

--- azure/virtual_machine/output.tf
+++ azure/virtual_machine/output.tf
@@ -1,6 +1,6 @@
-output "output" {
+output "virtual_machine" {
   value = {
-    "azure_vm_public_ip"   = azurerm_linux_virtual_machine.vm.public_ip_address
-    "azure_vm_private_ip"  = azurerm_linux_virtual_machine.vm.private_ip_address
+    "public_ip"  = azurerm_linux_virtual_machine.vm.public_ip_address
+    "private_ip" = azurerm_linux_virtual_machine.vm.private_ip_address
   }
 }
--- azure/virtual_network/output.tf
+++ azure/virtual_network/output.tf
@@ -1,6 +1,6 @@
 output "vnet" {
   value = {
-    "name"            = azurerm_virtual_network.vnet.name
-    "address_space"   = azurerm_virtual_network.vnet.address_space[0]
+    "name"          = azurerm_virtual_network.vnet.name
+    "address_space" = azurerm_virtual_network.vnet.address_space
   }
 }

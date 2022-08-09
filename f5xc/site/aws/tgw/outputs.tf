5c314593d8cf63aae9fdff05b9a095c362d8467b
cklewar@t-online.de <cklewar@t-online.de>
2022-08-09 02:24:58 +0200

    update aws tgw module

--- f5xc/site/aws/tgw/outputs.tf
+++ f5xc/site/aws/tgw/outputs.tf
@@ -12,10 +12,19 @@ output "tgw_subnet_slo" {
   value = {for k, v in data.aws_subnet.tgw_subnet_slo : k => v}
 }
 
-/*output "tgw_subnet_sli" {
-  value = {for k, v in data.aws_subnet.tgw_subnet_sli : k => v}
-}*/
-
 output "tgw_subnet_workload" {
   value = {for k, v in data.aws_subnet.tgw_subnet_workload : k => v}
 }
+
+output "tgw" {
+  value = {
+    tgw_id              = data.aws_ec2_transit_gateway.tgw.id
+    tgw_owner_id        = data.aws_ec2_transit_gateway.tgw.owner_id
+    ce_master_public_ip = data.aws_instance.ce_master.public_ip
+    //tgw_subnet_sli      = {for k, v in data.aws_subnet.tgw_subnet_sli : k => v}
+    tgw_subnet_slo      = {for k, v in data.aws_subnet.tgw_subnet_slo : k => v}
+    tgw_subnet_workload = {for k, v in data.aws_subnet.tgw_subnet_workload : k => v}
+
+  }
+}
+

ebf0216056fa871a48e7499844b2ba629369dd4a
cklewar@t-online.de <cklewar@t-online.de>
2022-08-09 02:20:13 +0200

    update site-mesh-group module

---
+++ f5xc/site-mesh-group/output.tf
@@ -0,0 +1,8 @@
+output "site-mesh-group" {
+  value = {
+    "name"        = volterra_site_mesh_group.site_mesh_group.name
+    "id"          = volterra_site_mesh_group.site_mesh_group.id
+    "type"        = volterra_site_mesh_group.site_mesh_group.type
+    "tunnel_type" = volterra_site_mesh_group.site_mesh_group.tunnel_type
+  }
+}

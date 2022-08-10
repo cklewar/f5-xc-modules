c2425f8917cebc85f9c4ded77df4b94ad58b1068
cklewar@t-online.de <cklewar@t-online.de>
2022-08-10 19:25:18 +0200

    update readme

--- f5xc/site/azure/versions.tf
+++ f5xc/site/azure/versions.tf
@@ -7,11 +7,6 @@ terraform {
       version = ">= 0.11.11"
     }
 
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.20.1"
-    }
-
     local = ">= 2.2.3"
     null  = ">= 3.1.1"
   }

ad1ced7a7fad734c9c0917670a5e2fa48eca8f3f
cklewar@t-online.de <cklewar@t-online.de>
2022-08-10 17:07:59 +0200

    update f5xc aws tgw module

--- f5xc/site/aws/tgw/data.tf
+++ f5xc/site/aws/tgw/data.tf
@@ -86,7 +86,7 @@ data "aws_subnet" "tgw_subnet_slo" {
   depends_on = [volterra_tf_params_action.aws_tgw_action]
   for_each   = var.f5xc_aws_tgw_az_nodes
 
-  cidr_block = contains(keys(var.f5xc_aws_tgw_az_nodes), "f5xc_aws_tgw_outside_subnet") ? var.f5xc_aws_tgw_az_nodes[each.key]["f5xc_aws_tgw_outside_subnet"] : var.f5xc_aws_tgw_az_nodes[each.key]["f5xc_aws_tgw_outside_existing_subnet_id"]
+  cidr_block = contains(keys(var.f5xc_aws_tgw_az_nodes[each.key]), "f5xc_aws_tgw_outside_subnet") ? var.f5xc_aws_tgw_az_nodes[each.key]["f5xc_aws_tgw_outside_subnet"] : var.f5xc_aws_tgw_az_nodes[each.key]["f5xc_aws_tgw_outside_existing_subnet_id"]
   vpc_id     = data.aws_vpc.tgw_vpc.id
 
   filter {

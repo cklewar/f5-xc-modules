5ce6eed0c476c19f09da6b0a692a7e7ea99d750d
cklewar@t-online.de <cklewar@t-online.de>
2022-08-12 13:06:37 +0200

    update f5xc smg module

--- f5xc/site-mesh-group/main.tf
+++ f5xc/site-mesh-group/main.tf
@@ -4,15 +4,15 @@ resource "volterra_site_mesh_group" "site_mesh_group" {
   tunnel_type = var.f5xc_tunnel_type
   type        = var.f5xc_site_2_site_connection_type
   description = var.f5xc_site_mesh_group_description
-
-  dynamic "hub_mesh" {
+  hub_mesh    = false
+  /*dynamic "hub_mesh" {
     for_each = var.f5xc_site_2_site_connection_type == var.f5xc_site_2_site_connection_type_hub_mesh ? [1] : []
     content {
       #name      = var.f5xc_site_site_hub_name
       #namespace = var.f5xc_namespace
       #tenant    = var.f5xc_tenant
     }
-  }
+  }*/
 
   dynamic "full_mesh" {
     for_each = var.f5xc_site_2_site_connection_type == var.f5xc_site_2_site_connection_type_full_mesh ? [1] : []

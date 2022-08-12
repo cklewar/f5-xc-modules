470eb3bf507cbe21a6be1a3da42c43b86538daed
cklewar@t-online.de <cklewar@t-online.de>
2022-08-12 14:12:52 +0200

    update f5xc smg module

--- aws/subnet/outputs.tf
+++ aws/subnet/outputs.tf
@@ -1,3 +1,7 @@
-output "aws_subnet_id" {
-  value = {for idx, val in aws_subnet.subnet : idx => val.id}
+#output "aws_subnet_id" {
+#  value = {for idx, val in aws_subnet.subnet : idx => val.id}
+#}
+
+output "aws_subnets" {
+  value = {for key, val in aws_subnet.subnet : name => val.name}
 }

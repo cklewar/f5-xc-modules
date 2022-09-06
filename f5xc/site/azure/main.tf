eae59a0d647f3fae2e7194d8dc65b55a07418c5d
cklewar@t-online.de <cklewar@t-online.de>
2022-09-06 11:51:19 +0200

    update azurerm version

--- f5xc/site/azure/main.tf
+++ f5xc/site/azure/main.tf
@@ -122,34 +122,19 @@ resource "volterra_azure_vnet_site" "site" {
           }
         }
       }
-    }
 
-    dynamic "hub" {
-      for_each = length(var.f5xc_azure_hub_spoke_vnets) > 0 ? var.f5xc_azure_hub_spoke_vnets : []
-      content {
-        spoke_vnets {
-          vnet = {
-            resource_group = hub.value.resource_group
-            vnet_name      = hub.value.vnet_name
-          }
-          auto   = hub.value.auto
-          labels = hub.value.lables
-        }
+      azure_certified_hw = var.f5xc_azure_ce_certified_hw[var.f5xc_azure_ce_gw_type]
+      local_control_plane {
+        no_local_control_plane = var.f5xc_azure_no_local_control_plane
       }
+
+      no_global_network        = var.f5xc_azure_no_global_network
+      no_outside_static_routes = var.f5xc_azure_no_outside_static_routes
+      no_inside_static_routes  = var.f5xc_azure_no_inside_static_routes
+      no_network_policy        = var.f5xc_azure_no_network_policy
+      no_forward_proxy         = var.f5xc_azure_no_forward_proxy
     }
   }
-
-  azure_certified_hw = var.f5xc_azure_ce_certified_hw[var.f5xc_azure_ce_gw_type]
-  local_control_plane {
-    no_local_control_plane = var.f5xc_azure_no_local_control_plane
-  }
-
-  no_global_network        = var.f5xc_azure_no_global_network
-  no_outside_static_routes = var.f5xc_azure_no_outside_static_routes
-  no_inside_static_routes  = var.f5xc_azure_no_inside_static_routes
-  no_network_policy        = var.f5xc_azure_no_network_policy
-  no_forward_proxy         = var.f5xc_azure_no_forward_proxy
-
 
   vnet {
     dynamic "new_vnet" {

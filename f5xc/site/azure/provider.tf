01b267508eefa65e4cccd7bfe2c6d44b0c20c78c
cklewar@t-online.de <cklewar@t-online.de>
2022-09-07 18:27:18 +0200

    update az login v2

--- f5xc/site/azure/provider.tf
+++ f5xc/site/azure/provider.tf
@@ -8,8 +8,8 @@ provider "volterra" {
 
 provider "azurerm" {
   features {}
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
-  tenant_id       = var.azure_tenant_id
-  subscription_id = var.azure_subscription_id
+  #client_id       = var.azure_client_id
+  #client_secret   = var.azure_client_secret
+  #tenant_id       = var.azure_tenant_id
+  #subscription_id = var.azure_subscription_id
 }

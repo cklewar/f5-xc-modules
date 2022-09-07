fe6b1c2d31f7d4e8538028f508061d42e8d5046b
cklewar@t-online.de <cklewar@t-online.de>
2022-09-07 18:11:24 +0200

    add azure cred vars

--- f5xc/site/azure/provider.tf
+++ f5xc/site/azure/provider.tf
@@ -11,5 +11,5 @@ provider "azurerm" {
   client_id       = var.azure_client_id
   client_secret   = var.azure_client_secret
   tenant_id       = var.azure_tenant_id
-  subscription_id = var.azure_subscription_ids
+  subscription_id = var.azure_subscription_id
 }

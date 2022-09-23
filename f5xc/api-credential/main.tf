4ff4ed64eb90711b00ff7b26b660ec62384b53c1
cklewar@t-online.de <cklewar@t-online.de>
2022-09-23 11:30:07 +0200

    add module api credential

--- f5xc/api-credential/main.tf
+++ f5xc/api-credential/main.tf
@@ -1,5 +1,5 @@
 resource "volterra_api_credential" "credential" {
-  name                  = var.f5xc_vk8s_api_credentials_name
+  name                  = var.f5xc_api_credentials_name
   api_credential_type   = var.f5xc_api_credential_type
   virtual_k8s_namespace = var.f5xc_namespace
   virtual_k8s_name      = var.f5xc_vk8s_name
---
+++ f5xc/api-credential/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.2.7"
+
+  required_providers {
+    volterra = {
+      source  = "volterraedge/volterra"
+      version = ">= 0.11.12"
+    }
+
+    local = ">= 2.2.3"
+    null  = ">= 3.1.1"
+  }
+}

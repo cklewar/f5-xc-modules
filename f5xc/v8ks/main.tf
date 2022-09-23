4f1d5062d5012e621a4d1db541aa77921d052d87
cklewar@t-online.de <cklewar@t-online.de>
2022-09-23 17:17:36 +0200

    update vk8s module

--- f5xc/v8ks/main.tf
+++ f5xc/v8ks/main.tf
@@ -13,25 +13,12 @@ resource "volterra_virtual_k8s" "vk8s" {
     }
   }
 
-  for_each = var.f5xc_vk8s_default_flavor_name != "" ? [1] : []
-  content {
-    name      = var.f5xc_vk8s_default_flavor_name
-    tenant    = var.f5xc_tenant
-    namespace = var.f5xc_namespace
-  }
-
-  provisioner "local-exec" {
-    command = "sleep $F5XC_VK8S_PROVISIONER_APPLY_TIMEOUT"
-    environment = {
-      F5XC_VK8S_PROVISIONER_APPLY_TIMEOUT = var.f5xc_vk8s_provisioner_apply_timeout
+  /*dynamic "default_flavor_ref" {
+    for_each = var.f5xc_vk8s_default_flavor_name != "" ? [1] : []
+    content {
+      name      = var.f5xc_vk8s_default_flavor_name
+      tenant    = var.f5xc_tenant
+      namespace = var.f5xc_namespace
     }
-  }
-
-  provisioner "local-exec" {
-    when    = destroy
-    command = "sleep $F5XC_VK8S_PROVISIONER_DESTROY_TIMEOUT"
-    environment = {
-      F5XC_VK8S_PROVISIONER_DESTROY_TIMEOUT = var.f5xc_vk8s_provisioner_destroy_timeout
-    }
-  }
+  }*/
 }

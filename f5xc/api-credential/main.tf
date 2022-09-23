704cdb74071f49200b40efb2a5e126c177c31486
cklewar@t-online.de <cklewar@t-online.de>
2022-09-23 12:01:07 +0200

    update module api credential

--- f5xc/api-credential/main.tf
+++ f5xc/api-credential/main.tf
@@ -1,8 +1,10 @@
 resource "volterra_api_credential" "credential" {
-  name                  = var.f5xc_api_credentials_name
-  api_credential_type   = var.f5xc_api_credential_type
-  virtual_k8s_namespace = var.f5xc_namespace
-  virtual_k8s_name      = var.f5xc_vk8s_name
+  name                    = var.f5xc_api_credentials_name
+  api_credential_type     = var.f5xc_api_credential_type
+  api_credential_password = var.f5xc_api_credential_type == var.f5xc_api_credential_type_api_certificate && var.f5xc_api_credential_password != "" ? var.f5xc_api_credential_password : null
+  virtual_k8s_namespace   = var.f5xc_api_credential_type == var.f5xc_api_credential_type_kube_config && var.f5xc_virtual_k8s_namespace != "" ? var.f5xc_virtual_k8s_namespace : null
+  virtual_k8s_name        = var.f5xc_api_credential_type == var.f5xc_api_credential_type_kube_config && var.f5xc_virtual_k8s_name != "" ? var.f5xc_virtual_k8s_name : null
+  expiry_days             = var.f5xc_api_credential_expiry_days != 0 ? var.f5xc_api_credential_expiry_days : null
   lifecycle {
     ignore_changes = [name]
   }

aa7f0792fdf158b1a37cfe6de150fef2ab84fef0
cklewar@t-online.de <cklewar@t-online.de>
2022-09-28 14:32:35 +0200

    rename vars

--- aws/ec2/outputs.tf
+++ aws/ec2/outputs.tf
@@ -1,9 +1,8 @@
 output "aws_ec2_instance" {
   value = {
     "script_file"        = var.aws_ec2_instance_script_file
-    "script_source"      = abspath(format("../modules/ec2/_out/%s", format("%s.sh", var.aws_ec2_instance_script_file)))
-    "script_destination" = format("/tmp/%s", format("%s.sh", var.aws_ec2_instance_script_file))
-    "userdata_source"    = abspath(format("../modules/ec2/userdata/%s", var.aws_ec2_instance_script_file))
+    "script_source"      = format("%s%s", var.rendered_template_output_path, var.aws_ec2_instance_script_file)
+    "script_destination" = format("/tmp/%s", var.aws_ec2_instance_script_file)
     "public_dns"         = aws_instance.instance.public_dns
     "public_ip"          = aws_eip.eip.public_ip
     "private_ip"         = aws_network_interface.private.private_ip

a76ff55421ef1a61dc54dbd6b3277639a3231eb6
cklewar@t-online.de <cklewar@t-online.de>
2022-10-05 17:46:19 +0200

    update azure vnet module

--- azure/virtual_network/main.tf
+++ azure/virtual_network/main.tf
@@ -3,4 +3,15 @@ resource "azurerm_virtual_network" "vnet" {
   location            = var.azure_region
   address_space       = [var.azure_vnet_primary_ipv4]
   resource_group_name = var.azure_vnet_resource_group_name
+  dns_servers         = var.azure_vnet_dns_servers
+  tags                = var.custom_tags
+
+  dynamic "subnet" {
+    for_each = length(var.azure_vnet_subnets) > 0 ? var.azure_vnet_subnets : []
+    content {
+      name           = subnet.value.name
+      address_prefix = subnet.value.address_prefix
+      security_group = subnet.value.security_group
+    }
+  }
 }

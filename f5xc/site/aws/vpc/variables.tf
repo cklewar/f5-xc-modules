cccda045c62c5a27d442b1809c11ef90f92c0a76
cklewar@t-online.de <cklewar@t-online.de>
2022-10-10 14:10:44 +0200

    0.11.14

--- f5xc/site/aws/vpc/variables.tf
+++ f5xc/site/aws/vpc/variables.tf
@@ -1,23 +1,3 @@
-variable "f5xc_api_url" {
-  type = string
-}
-
-variable "f5xc_api_cert" {
-  default = ""
-}
-
-variable "f5xc_api_key" {
-  default = ""
-}
-
-variable "f5xc_api_ca_cert" {
-  default = ""
-}
-
-variable "f5xc_api_p12_file" {
-  type = string
-}
-
 variable "f5xc_tenant" {
   type = string
 }

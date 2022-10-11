f96a9b56b7ebb1a917ebe143015b710b885cbce8
cklewar@t-online.de <cklewar@t-online.de>
2022-10-11 17:14:57 +0200

    update f5xc lb modules

--- f5xc/loadbalancer/tcp/variables.tf
+++ f5xc/loadbalancer/tcp/variables.tf
@@ -6,96 +6,94 @@ variable "f5xc_tenant" {
   type = string
 }
 
-variable "f5xc_http_loadbalancer_name" {
+variable "f5xc_tcp_lb_name" {
   type = string
 }
 
-variable "f5xc_http_loadbalancer_advertise_on_public_default_vip" {
+variable "f5xc_tcp_lb_advertise_on_public_default_vip" {
   type    = bool
   default = true
 }
 
-variable "f5xc_http_loadbalancer_do_not_advertise" {
+variable "f5xc_tcp_lb_do_not_advertise" {
   type    = bool
   default = false
 }
 
-variable "f5xc_http_loadbalancer_advertise_on_public" {
+variable "f5xc_tcp_lb_advertise_on_public" {
   type    = bool
   default = false
 }
 
-variable "f5xc_http_loadbalancer_advertise_custom" {
+variable "f5xc_tcp_lb_advertise_custom" {
   type    = bool
   default = false
 }
 
-variable "f5xc_http_loadbalancer_disable_api_definition" {
+variable "f5xc_tcp_lb_disable_api_definition" {
   type    = bool
   default = true
 }
 
-variable "f5xc_http_loadbalancer_no_challenge" {
+variable "f5xc_tcp_lb_no_challenge" {
   type    = bool
   default = true
 }
 
-variable "f5xc_http_loadbalancer_domains" {
+variable "f5xc_tcp_lb_domains" {
   type = list(string)
 }
 
-variable "f5xc_http_loadbalancer_round_robin" {
+variable "f5xc_tcp_lb_round_robin" {
   type    = bool
   default = true
 }
 
-variable "f5xc_http_loadbalancer_least_active" {
+variable "f5xc_tcp_lb_least_active" {
   type    = bool
   default = false
 }
 
-variable "f5xc_http_loadbalancer_random" {
+variable "f5xc_tcp_lb_random" {
   type    = bool
   default = false
 }
 
-variable "f5xc_http_loadbalancer_source_ip_stickiness" {
+variable "f5xc_tcp_lb_source_ip_stickiness" {
   type    = bool
   default = false
 }
 
-variable "f5xc_http_loadbalancer_cookie_stickiness" {
+variable "f5xc_tcp_lb_cookie_stickiness" {
   type    = bool
   default = false
 }
 
-variable "f5xc_http_loadbalancer_ring_hash" {
+variable "f5xc_tcp_lb_ring_hash" {
   type    = bool
   default = false
 }
 
-variable "f5xc_http_loadbalancer_disable_rate_limit" {
+variable "f5xc_tcp_lb_disable_rate_limit" {
   type    = bool
   default = true
 }
 
-variable "f5xc_http_loadbalancer_user_id_client_ip" {
+variable "f5xc_tcp_lb_user_id_client_ip" {
   type    = bool
   default = true
 }
 
-variable "f5xc_http_loadbalancer_disable_waf" {
+variable "f5xc_tcp_lb_disable_waf" {
   type    = bool
   default = true
 }
 
-variable "f5xc_http_loadbalancer_port" {
+variable "f5xc_tcp_lb_port" {
   type = string
 }
 
-variable "f5xc_http_loadbalancer_dns_volterra_managed" {
+variable "f5xc_tcp_lb_dns_volterra_managed" {
   type    = bool
   default = true
 }
-=======
->>>>>>> 368a486 (0.11.33)

3294bc3cb35f2fb0657cd077133c5f48856522d4
cklewar@t-online.de <cklewar@t-online.de>
2022-10-13 22:30:40 +0200

    update azure module

--- f5xc/site/azure/output.tf
+++ f5xc/site/azure/output.tf
@@ -2,7 +2,10 @@ output "vnet" {
   value = {
     name                = volterra_azure_vnet_site.site.name
     id                  = volterra_azure_vnet_site.site.id
+    sli                 = data.azurerm_network_interface.sli.private_ip_address
+    slo                 = data.azurerm_network_interface.slo.private_ip_address
     region              = volterra_azure_vnet_site.site.azure_region
+    public_ip           = data.azurerm_public_ip.pib.ip_address
     machine_type        = volterra_azure_vnet_site.site.machine_type
     vnet_resource_group = volterra_azure_vnet_site.site.resource_group
     params              = volterra_tf_params_action.azure_vnet_action

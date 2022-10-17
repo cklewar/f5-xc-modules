0c8e9eb30bcd6ea352b9bbb01a1653376146734f
cklewar@t-online.de <cklewar@t-online.de>
2022-10-17 16:15:55 +0200

    update vpc module

--- aws/network_interface/variables.tf
+++ aws/network_interface/variables.tf
@@ -1,29 +1,3 @@
-variable "aws_security_group_name" {
-  type = string
-}
-
-variable "aws_vpc_id" {
-  type = string
-}
-
-variable "aws_security_group_rule_egress" {
-  type = list(object({
-    from_port   = number
-    to_port     = number
-    protocol    = any
-    cidr_blocks = list(string)
-  }))
-}
-
-variable "aws_security_group_rule_ingress" {
-  type = list(object({
-    from_port   = number
-    to_port     = number
-    protocol    = any
-    cidr_blocks = list(string)
-  }))
-}
-
 variable "aws_interface_security_groups" {
   type = list(string)
 }

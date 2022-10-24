16e49dc04c316f2785ae9e28f0da276fb9e7b033
cklewar@t-online.de <cklewar@t-online.de>
2022-10-24 11:15:47 +0200

    update f5xc aws tgw module

--- f5xc/site/aws/tgw/outputs.tf
+++ f5xc/site/aws/tgw/outputs.tf
@@ -1,12 +1,13 @@
 output "f5xc_aws_tgw" {
   value = {
-    tgw_id                  = data.aws_ec2_transit_gateway.tgw.id
-    tgw_owner_id            = data.aws_ec2_transit_gateway.tgw.owner_id
-    ce_master_public_ip     = data.aws_instance.ce_master.public_ip
+    id                  = data.aws_ec2_transit_gateway.tgw.id
+    name                = volterra_aws_tgw_site.site.name
+    owner_id            = data.aws_ec2_transit_gateway.tgw.owner_id
+    ce_master_public_ip = data.aws_instance.ce_master.public_ip
     //tgw_subnet_sli      = {for k, v in data.aws_subnet.tgw_subnet_sli : k => v}
     //tgw_subnet_slo_existing = {for k, v in data.aws_subnet.tgw_subnet_slo : k => v}
     //tgw_subnet_workload     = {for k, v in data.aws_subnet.tgw_subnet_workload : k => v}
-    params                  = volterra_tf_params_action.aws_tgw_action
+    params              = volterra_tf_params_action.aws_tgw_action
   }
 }
 

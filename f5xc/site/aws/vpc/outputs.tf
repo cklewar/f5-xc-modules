4702991deb13c250348ed876cc694179bbe50207
cklewar@t-online.de <cklewar@t-online.de>
2022-11-03 15:41:08 +0100

    update f5xc aws vcp module

--- f5xc/site/aws/vpc/outputs.tf
+++ f5xc/site/aws/vpc/outputs.tf
@@ -1,15 +1,32 @@
 output "f5xc_aws_vpc" {
   value = {
-    name               = volterra_aws_vpc_site.site.name
-    id                 = volterra_aws_vpc_site.site.id
-    sli_ip             = var.f5xc_aws_ce_gw_type == var.f5xc_nic_type_multi_nic ? data.aws_network_interface.sli[0].private_ip : null
-    slo_ip             = data.aws_network_interface.slo.private_ip
-    region             = volterra_aws_vpc_site.site.aws_region
+    id     = volterra_aws_vpc_site.site.id
+    name   = volterra_aws_vpc_site.site.name
+    region = volterra_aws_vpc_site.site.aws_region
+    params = volterra_tf_params_action.aws_vpc_action
+    vpc_id = data.aws_vpc.vpc.id
+    nodes  = {
+    for node in data.aws_instance.nodes : node.tags["Name"] => {
+      id         = node.id
+      interfaces = {
+        slo = {
+          id               = data.aws_network_interface.slo.*.id
+          private_ip       = data.aws_network_interface.slo.*.private_ip
+          private_dns_name = data.aws_network_interface.slo.*.private_dns_name
+          public_ip        = data.aws_network_interface.slo.*.association[0][0].public_ip
+          public_dns_name  = data.aws_network_interface.slo.*.association[0][0].public_dns_name
+          subnet_id        = data.aws_network_interface.slo.*.subnet_id
+        }
+        sli = {
+          id               = data.aws_network_interface.sli.*.id
+          private_ip       = data.aws_network_interface.sli.*.private_ip
+          private_dns_name = data.aws_network_interface.sli.*.private_dns_name
+          subnet_id        = data.aws_network_interface.sli.*.subnet_id
+        }
+      }
+    }
+    }
     instance_type      = volterra_aws_vpc_site.site.instance_type
-    public_ip          = data.aws_network_interface.slo.association[0]["public_ip"]
-    params             = volterra_tf_params_action.aws_vpc_action
-    vpc_id             = data.aws_vpc.vpc.id
-    workload_subnet_id = var.f5xc_aws_ce_gw_type == var.f5xc_nic_type_multi_nic ? data.aws_subnet.workload[0].id : null
+    workload_subnet_id = var.f5xc_aws_ce_gw_type == var.f5xc_nic_type_multi_nic ? data.aws_subnets.workload[0].ids : null
   }
 }
-

b52b7f19c8044e052cfc822776c89b2cb2f13ff4
cklewar@t-online.de <cklewar@t-online.de>
2022-11-04 15:07:59 +0100

    update

--- f5xc/blindfold/data.tf
+++ f5xc/blindfold/data.tf
@@ -1,15 +1,16 @@
-data "http" "policy_document" {
-  depends_on = [null_resource.create_secret_policy_rule, null_resource.create_secret_policys]
-  url        = format("%s/%s", var.f5xc_api_url, var.f5xc_uri_secret_management_secret_policys_policy_document)
+/*data "http" "policy_document" {
+  # depends_on = [null_resource.create_secret_policy_rule, null_resource.create_secret_policys]
+  depends_on = [null_resource.action_secret_policy_rule]
+  url        = format("%s/%s", var.f5xc_api_url, local.f5xc_secret_management_secret_policy_rules_get_uri)
 
   request_headers = {
     Authorization           = format("APIToken %s", var.f5xc_api_token)
     Accept                  = "application/json"
     x-volterra-apigw-tenant = var.f5xc_namespace
   }
-}
+}*/
 
-data "http" "public_key" {
+/*data "http" "public_key" {
   depends_on = [null_resource.create_secret_policy_rule, null_resource.create_secret_policys]
   url        = format("%s/%s", var.f5xc_api_url, var.f5xc_uri_secret_management_public_key)
 
@@ -18,4 +19,4 @@ data "http" "public_key" {
     Accept                  = "application/json"
     x-volterra-apigw-tenant = var.f5xc_namespace
   }
-}
+}*/

c551bc9e85243245672e76b1ae4022faa4c99443
cklewar@t-online.de <cklewar@t-online.de>
2022-11-04 12:13:53 +0100

    update f5xc aws vcp module

--- f5xc/site/aws/vpc/data.tf
+++ f5xc/site/aws/vpc/data.tf
@@ -1,10 +1,13 @@
-data "aws_instances" "nodes" {
-  depends_on           = [volterra_aws_vpc_site.site, module.site_wait_for_online]
-  instance_state_names = ["running"]
+data "aws_instance" "master-0" {
+  depends_on = [module.site_wait_for_online]
 
   filter {
+    name   = "instance-state-name"
+    values = ["running"]
+  }
+  filter {
     name   = "tag:Name"
-    values = ["master-*"]
+    values = ["master-0"]
   }
   filter {
     name   = "tag:ves-io-site-name"
@@ -12,12 +15,52 @@ data "aws_instances" "nodes" {
   }
 }
 
-data "aws_network_interface" "slo" {
-  depends_on = [volterra_aws_vpc_site.site, module.site_wait_for_online]
+data "aws_instance" "master-1" {
+  depends_on = [module.site_wait_for_online]
+  count      = length(var.f5xc_aws_vpc_az_nodes) >= 2 ? 1 : 0
+
+  filter {
+    name   = "instance-state-name"
+    values = ["running"]
+  }
+  filter {
+    name   = "tag:Name"
+    values = ["master-1"]
+  }
+  filter {
+    name   = "tag:ves-io-site-name"
+    values = [var.f5xc_aws_vpc_site_name]
+  }
+}
+
+data "aws_instance" "master-2" {
+  depends_on = [module.site_wait_for_online]
+  count      = length(var.f5xc_aws_vpc_az_nodes) >= 2 ? 1 : 0
+
+  filter {
+    name   = "instance-state-name"
+    values = ["running"]
+  }
+  filter {
+    name   = "tag:Name"
+    values = ["master-2"]
+  }
+  filter {
+    name   = "tag:ves-io-site-name"
+    values = [var.f5xc_aws_vpc_site_name]
+  }
+}
+
+data "aws_network_interface" "master-0-slo" {
+  depends_on = [module.site_wait_for_online]
 
   filter {
     name   = "attachment.instance-id"
-    values = data.aws_instances.nodes.ids
+    values = [data.aws_instance.master-0.id]
+  }
+  filter {
+    name   = "tag:ves-io-site-name"
+    values = [var.f5xc_aws_vpc_site_name]
   }
   filter {
     name   = "tag:ves.io/interface-type"
@@ -25,13 +68,89 @@ data "aws_network_interface" "slo" {
   }
 }
 
-data "aws_network_interface" "sli" {
+data "aws_network_interface" "master-0-sli" {
   depends_on = [module.site_wait_for_online]
   count      = var.f5xc_aws_ce_gw_type == var.f5xc_nic_type_multi_nic ? 1 : 0
 
   filter {
     name   = "attachment.instance-id"
-    values = data.aws_instances.nodes.ids
+    values = [data.aws_instance.master-0.id]
+  }
+  filter {
+    name   = "tag:ves-io-site-name"
+    values = [var.f5xc_aws_vpc_site_name]
+  }
+  filter {
+    name   = "tag:ves.io/interface-type"
+    values = ["site-local-inside"]
+  }
+}
+
+data "aws_network_interface" "master-1-slo" {
+  depends_on = [module.site_wait_for_online]
+  count      = length(var.f5xc_aws_vpc_az_nodes) >= 2 ? 1 : 0
+
+  filter {
+    name   = "attachment.instance-id"
+    values = [data.aws_instance.master-1[0].id]
+  }
+  filter {
+    name   = "tag:ves-io-site-name"
+    values = [var.f5xc_aws_vpc_site_name]
+  }
+  filter {
+    name   = "tag:ves.io/interface-type"
+    values = ["site-local-outside"]
+  }
+}
+
+data "aws_network_interface" "master-1-sli" {
+  depends_on = [module.site_wait_for_online]
+  count      = length(var.f5xc_aws_vpc_az_nodes) >= 2 && var.f5xc_aws_ce_gw_type == var.f5xc_nic_type_multi_nic ? 1 : 0
+
+  filter {
+    name   = "attachment.instance-id"
+    values = [data.aws_instance.master-1[0].id]
+  }
+  filter {
+    name   = "tag:ves-io-site-name"
+    values = [var.f5xc_aws_vpc_site_name]
+  }
+  filter {
+    name   = "tag:ves.io/interface-type"
+    values = ["site-local-inside"]
+  }
+}
+
+data "aws_network_interface" "master-2-slo" {
+  depends_on = [module.site_wait_for_online]
+  count      = length(var.f5xc_aws_vpc_az_nodes) >= 2 ? 1 : 0
+
+  filter {
+    name   = "attachment.instance-id"
+    values = [data.aws_instance.master-2[0].id]
+  }
+  filter {
+    name   = "tag:ves-io-site-name"
+    values = [var.f5xc_aws_vpc_site_name]
+  }
+  filter {
+    name   = "tag:ves.io/interface-type"
+    values = ["site-local-outside"]
+  }
+}
+
+data "aws_network_interface" "master-2-sli" {
+  depends_on = [module.site_wait_for_online]
+  count      = length(var.f5xc_aws_vpc_az_nodes) >= 2 && var.f5xc_aws_ce_gw_type == var.f5xc_nic_type_multi_nic ? 1 : 0
+
+  filter {
+    name   = "attachment.instance-id"
+    values = [data.aws_instance.master-2[0].id]
+  }
+  filter {
+    name   = "tag:ves-io-site-name"
+    values = [var.f5xc_aws_vpc_site_name]
   }
   filter {
     name   = "tag:ves.io/interface-type"

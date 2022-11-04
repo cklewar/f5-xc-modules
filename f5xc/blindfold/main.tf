8e4c39d232a38c291274b1dec5065b1cc0569d8b
cklewar@t-online.de <cklewar@t-online.de>
2022-11-04 12:58:13 +0100

    update f5xc blindfold module

--- f5xc/blindfold/main.tf
+++ f5xc/blindfold/main.tf
@@ -1,10 +1,11 @@
 resource "null_resource" "create_secret_policy_rule" {
   triggers = {
-    manifest_sha1 = sha1(local.f5xc_blindfold_policy_rule_payload)
+    name          = var.f5xc_blindfold_secret_policy_rule_name
     api_url       = var.f5xc_api_url
     api_token     = var.f5xc_api_token
     namespace     = var.f5xc_namespace
     delete_uri    = var.f5xc_blindfold_secret_policy_rule_delete_uri
+    manifest_sha1 = sha1(local.f5xc_blindfold_policy_rule_payload)
   }
 
   provisioner "local-exec" {
@@ -17,8 +18,9 @@ resource "null_resource" "create_secret_policy_rule" {
     command     = "${path.module}/scripts/delete.sh"
     on_failure  = fail
     environment = {
-      api_token  = self.triggers.api_token
+      name       = self.triggers.name
       api_url    = self.triggers.api_url
+      api_token  = self.triggers.api_token
       namespace  = self.triggers.namespace
       delete_uri = self.triggers.delete_uri
     }
@@ -28,11 +30,12 @@ resource "null_resource" "create_secret_policy_rule" {
 resource "null_resource" "create_secret_policys" {
   depends_on = [null_resource.create_secret_policy_rule]
   triggers   = {
-    manifest_sha1 = sha1(local.f5xc_blindfold_policys_payload)
+    name          = var.f5xc_blindfold_secret_policy_name
     api_url       = var.f5xc_api_url
     api_token     = var.f5xc_api_token
     namespace     = var.f5xc_namespace
     delete_uri    = local.f5xc_blindfold_service_policy_delete_uri
+    manifest_sha1 = sha1(local.f5xc_blindfold_policys_payload)
   }
 
   provisioner "local-exec" {
@@ -45,8 +48,9 @@ resource "null_resource" "create_secret_policys" {
     command     = "${path.module}/scripts/delete.sh"
     on_failure  = fail
     environment = {
-      api_token  = self.triggers.api_token
+      name       = self.triggers.name
       api_url    = self.triggers.api_url
+      api_token  = self.triggers.api_token
       namespace  = self.triggers.namespace
       delete_uri = self.triggers.delete_uri
     }

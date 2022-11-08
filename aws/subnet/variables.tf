86215c34eef03b4307fad0154fd8541eaed45c75
cklewar@t-online.de <cklewar@t-online.de>
2022-11-08 13:35:42 +0100

    update f5xc blindfold module

--- aws/subnet/variables.tf
+++ aws/subnet/variables.tf
@@ -2,12 +2,6 @@ variable "aws_vpc_id" {
   type = string
 }
 
-variable "custom_tags" {
-  description = "Custom tags to set on resources"
-  type        = map(string)
-  default     = {}
-}
-
 variable "aws_vpc_subnets" {
   type = list(object({
     map_public_ip_on_launch = bool

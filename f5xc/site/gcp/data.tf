945ddb89f1d4a367752c30dbff1dd3d36dfbf060
cklewar@t-online.de <cklewar@t-online.de>
2022-11-14 11:27:08 +0100

    v0.11.14

--- f5xc/site/gcp/data.tf
+++ f5xc/site/gcp/data.tf
@@ -1,10 +1,5 @@
-data "external" "instance" {
-  depends_on = [module.site_wait_for_online]
-  program = ["./scripts/script.sh"]
-}
-
 data "google_compute_instance" "instance" {
   depends_on = [module.site_wait_for_online]
-  name = data.external.instance.result.instance_name
+  name = regex(local.f5xc_gcp_instance_name_pattern, volterra_tf_params_action.gcp_vpc_action.tf_output)[0]
   zone = element(var.f5xc_gcp_zone_names, 0)
 }
--- f5xc/site/gcp/scripts/script.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-INSTANCE_NAME=$(/opt/homebrew/Cellar/hcl2json/0.3.5/bin/hcl2json test.hcl | jq '.instance_names[0]')
-MASTER_PRIVATE_IP_ADDRESS=$(/opt/homebrew/Cellar/hcl2json/0.3.5/bin/hcl2json test.hcl | jq '.master_private_ip_address.'"$INSTANCE_NAME")
-MASTER_PUBLIC_IP_ADDRESS=$(/opt/homebrew/Cellar/hcl2json/0.3.5/bin/hcl2json test.hcl | jq '.master_public_ip_address.'"$INSTANCE_NAME")
-jq -n --argjson instance_name "$INSTANCE_NAME" --argjson master_private_ip_address "$MASTER_PRIVATE_IP_ADDRESS" --argjson master_public_ip_address "$MASTER_PUBLIC_IP_ADDRESS" '{"instance_name":$instance_name, "master_private_ip_address":$master_private_ip_address, "master_public_ip_address": $master_public_ip_address}'

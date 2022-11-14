840e16f31eb21ac90c386735c491299ded5a56b1
cklewar@t-online.de <cklewar@t-online.de>
2022-11-14 11:25:52 +0100

    v0.11.16

--- f5xc/site/gcp/output.tf
+++ f5xc/site/gcp/output.tf
@@ -1,13 +1,12 @@
 output "gcp_vpc" {
   value = {
-    name              = volterra_gcp_vpc_site.site.name
-    id                = volterra_gcp_vpc_site.site.id
-    region            = volterra_gcp_vpc_site.site.gcp_region
-    slo_ip            = data.google_compute_instance.instance.network_interface.0.network_ip
-    sli_ip            = data.google_compute_instance.instance.network_interface.1.network_ip
-    params            = volterra_tf_params_action.gcp_vpc_action
-    public_ip         = data.google_compute_instance.instance.network_interface.0.access_config.0.nat_ip
-    instance_type     = volterra_gcp_vpc_site.site.instance_type
-    network_interface = data.google_compute_instance.instance.network_interface
+    name          = volterra_gcp_vpc_site.site.name
+    id            = volterra_gcp_vpc_site.site.id
+    region        = volterra_gcp_vpc_site.site.gcp_region
+    slo_ip        = data.google_compute_instance.instance.network_interface.0.network_ip
+    sli_ip        = data.google_compute_instance.instance.network_interface.1.network_ip
+    params        = volterra_tf_params_action.gcp_vpc_action
+    public_ip     = data.google_compute_instance.instance.network_interface.0.access_config.0.nat_ip
+    instance_type = volterra_gcp_vpc_site.site.instance_type
   }
 }

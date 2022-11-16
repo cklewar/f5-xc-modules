6ad4eca3609ee3e6cd496b19b31b36272efe29d8
cklewar@t-online.de <cklewar@t-online.de>
2022-11-16 13:06:36 +0100

    update f5xc api credential module

--- f5xc/v8ks/output.tf
+++ f5xc/v8ks/output.tf
@@ -2,6 +2,6 @@ output "vk8s" {
   value = {
     "name"        = volterra_virtual_k8s.vk8s.name
     "id"          = volterra_virtual_k8s.vk8s.id
-    "kube_config" = var.f5xc_create_k8s_creds == true && length(module.api_credential_kubeconfig) > 0 ? base64decode(module.api_credential_kubeconfig.*.api_credential[0]["data"]) : null
+    "k8s_config" = var.f5xc_create_k8s_creds == true && length(module.api_credential_kubeconfig) > 0 ? base64decode(module.api_credential_kubeconfig.*.api_credential[0]["data"]) : null
   }
 }

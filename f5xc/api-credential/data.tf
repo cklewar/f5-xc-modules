e031748e626a3da15d6e8301f232c70c8c272e37
cklewar@t-online.de <cklewar@t-online.de>
2022-11-16 16:51:54 +0100

    update module

--- f5xc/api-credential/data.tf
+++ f5xc/api-credential/data.tf
@@ -1,14 +1,17 @@
 data "local_file" "response" {
-  count    = fileexists(null_resource.apply_credential.triggers.filename) ? 1 : 0
-  filename = null_resource.apply_credential.triggers.filename
+  depends_on = [null_resource.apply_credential]
+  count      = fileexists(null_resource.apply_credential.triggers.filename) ? 1 : 0
+  filename   = null_resource.apply_credential.triggers.filename
 }
 
 data "http" "credential" {
-  count    = fileexists(null_resource.apply_credential.triggers.filename) ? 1 : 0
-  url = format("%s/%s/%s", var.f5xc_api_url, local.credential_get_uri, jsondecode(data.local_file.response.*.content).name)
+  count = fileexists(null_resource.apply_credential.triggers.filename) ? 1 : 0
+  # https://playground.staging.volterra.us/api/web/namespaces/system/api_credentials/f5xc-vk8s-credential-01-cwcjeirw
+  url   = format("%s/%s/%s", var.f5xc_api_url, local.credential_get_uri, jsondecode(data.local_file.response.*.content[0]).name)
+  # url = format("https://playground.staging.volterra.us/api/web/namespaces/system/api_credentials/%s", )
 
   request_headers = {
-    Content-Type = "application/json"
+    Content-Type  = "application/json"
     Authorization = format("APIToken %s", var.f5xc_api_token)
   }
 }

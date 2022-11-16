4eb83eea7f835be2f5f793a8fe99cd3595cff7b2
cklewar@t-online.de <cklewar@t-online.de>
2022-11-16 15:54:39 +0100

    update f5xc api credential module

--- f5xc/api-credential/versions.tf
+++ f5xc/api-credential/versions.tf
@@ -2,11 +2,6 @@ terraform {
   required_version = ">= 1.3.0"
 
   required_providers {
-    volterra = {
-      source  = "volterraedge/volterra"
-      version =  "= 0.11.14"
-    }
-
     http = {
       source  = "hashicorp/http"
       version = ">= 3.1.0"

0223866c498ddb0fd7c914d81fa9b2bb2e16ada5
cklewar@t-online.de <cklewar@t-online.de>
2022-11-16 11:29:55 +0100

    update f5xc vk8s module

--- f5xc/v8ks/output.tf
+++ f5xc/v8ks/output.tf
@@ -1,6 +1,7 @@
 output "vk8s" {
   value = {
-    "name" = volterra_virtual_k8s.vk8s.name
-    "id"   = volterra_virtual_k8s.vk8s.id
+    "name"        = volterra_virtual_k8s.vk8s.name
+    "id"          = volterra_virtual_k8s.vk8s.id
+    "kube_config" = var.f5xc_create_k8s_creds == true ? base64decode(module.api_credential_kubeconfig.api_credential["data"]) : null
   }
 }

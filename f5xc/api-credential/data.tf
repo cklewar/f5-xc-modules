ae51e745208bf8d47145f49ff3cb63cd002170b8
cklewar@t-online.de <cklewar@t-online.de>
2022-11-17 00:02:12 +0100

    update f5xc api cred module

--- f5xc/api-credential/data.tf
+++ f5xc/api-credential/data.tf
@@ -5,6 +5,7 @@ data "local_file" "response" {
 }
 
 data "http" "credential" {
+  depends_on = [null_resource.apply_credential]
   count = fileexists(null_resource.apply_credential.triggers.filename) ? 1 : 0
   url   = format("%s/%s/%s", var.f5xc_api_url, local.credential_get_uri, jsondecode(data.local_file.response.*.content[0]).name)
 

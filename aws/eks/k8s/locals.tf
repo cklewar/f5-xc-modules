0d925214a940bb2e8d813124f320deb0c0d3375f
cklewar@t-online.de <cklewar@t-online.de>
2022-11-22 00:29:18 +0100

    rework eks module

--- aws/eks/k8s/locals.tf
+++ aws/eks/k8s/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  kubeconfig = templatefile("${path.module}/templates/kubeconfig.tpl", {
+  kubeconfig = templatefile("${path.module}/templates/kubeconfig.tftpl", {
     cluster_name = var.cluster_name,
     clusterca    = data.aws_eks_cluster.default.certificate_authority[0].data,
     endpoint     = data.aws_eks_cluster.default.endpoint,

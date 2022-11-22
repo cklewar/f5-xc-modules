ef5132e07904b04cd9cea40e96d4de61d036bf5a
cklewar <cklewar@t-online.de>
2022-11-23 00:59:17 +0100

    0.11.16 v27

--- f5xc/api-credential/data.tf
+++ f5xc/api-credential/data.tf
@@ -5,7 +5,7 @@ data "local_file" "response" {
 }
 
 data "http" "credential" {
-  count = fileexists(null_resource.apply_credential.triggers.filename) ? 1 : 0
+  count = fileexists(null_resource.apply_credential.triggers.filename) == true ? 1 : 0
   url   = format("%s/%s/%s", var.f5xc_api_url, local.credential_get_uri, jsondecode(data.local_file.response.*.content[0]).name)
 
   request_headers = {

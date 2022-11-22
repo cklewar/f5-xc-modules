3715e608cf6ebffc7173a277705666d10d8af133
cklewar@t-online.de <cklewar@t-online.de>
2022-11-22 01:29:45 +0100

    rework eks module

--- aws/eks/main.tf
+++ aws/eks/main.tf
@@ -1,5 +1,11 @@
 module "aws_eks" {
   source               = "./eks"
+  owner                = var.owner
+  aws_region           = var.aws_region
+  aws_az_name          = var.aws_az_name
+  aws_vpc_subnet_a     = var.aws_vpc_subnet_a
+  aws_vpc_subnet_b     = var.aws_vpc_subnet_b
+  aws_vpc_cidr_block   = var.aws_vpc_cidr_block
   aws_eks_cluster_name = var.aws_eks_cluster_name
 }
 

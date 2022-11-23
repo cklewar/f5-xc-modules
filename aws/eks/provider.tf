bd9bcc2ea8a71b4f0873015fc2bbb4fe3dcfba5f
cklewar <cklewar@t-online.de>
2022-11-23 11:39:20 +0100

    update vk8s module

--- aws/eks/provider.tf
+++ aws/eks/provider.tf
@@ -3,11 +3,3 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.default.token
 }
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.default.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.default.token
-  }
-}

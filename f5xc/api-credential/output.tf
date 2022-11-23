209e818ea01341f93bf9b594c35bfdcf9de2b090
cklewar <cklewar@t-online.de>
2022-11-23 02:28:52 +0100

    0.11.16 v43

--- f5xc/api-credential/output.tf
+++ f5xc/api-credential/output.tf
@@ -18,8 +18,8 @@ output "api_credential" {
     "expiration_timestamp"  = length(data.http.credential.*.response_body) > 0 ? jsondecode(data.http.credential.*.response_body[0]).object.spec.gc_spec.expiration_timestamp : null
     "k8s_conf"              = var.f5xc_api_credential_type == var.f5xc_api_credential_type_kube_config && length(data.local_file.response) > 0 ? base64decode(jsondecode(data.local_file.response[0].content).data) : ""
     "api_certificate"       = var.f5xc_api_credential_type == var.f5xc_api_credential_type_api_certificate && length(data.local_file.response) > 0 ? jsondecode(data.local_file.response.*.content[0]).data : ""
-    "data"                  = fileexists(null_resource.apply_credential.triggers.filename)
-    "data1"                 = fileexists("./_out/response.json")
+    "data1"                 = fileexists(null_resource.apply_credential.triggers.filename)
+    "data2"                 = fileexists("./_out/response.json")
     "data3"                 = null_resource.apply_credential.triggers.filename
   }
 }

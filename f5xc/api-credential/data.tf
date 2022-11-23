7296b3f2c9ff5ad2014b6f9e0bd6c859d11ecef4
cklewar <cklewar@t-online.de>
2022-11-23 03:04:12 +0100

    0.11.16 v50

--- f5xc/api-credential/data.tf
+++ f5xc/api-credential/data.tf
@@ -1,11 +1,11 @@
 data "local_file" "response" {
   depends_on = [null_resource.apply_credential]
-  count      = fileexists(null_resource.apply_credential.triggers.filename) == true ? 1 : 0
+  # count      = fileexists(null_resource.apply_credential.triggers.filename) == true ? 1 : 0
   filename   = null_resource.apply_credential.triggers.filename
 }
 
 data "http" "credential" {
-  count = fileexists(null_resource.apply_credential.triggers.filename) == true ? 1 : 0
+  # count = fileexists(null_resource.apply_credential.triggers.filename) == true ? 1 : 0
   url   = format("%s/%s/%s", var.f5xc_api_url, local.credential_get_uri, jsondecode(data.local_file.response.*.content[0]).name)
 
   request_headers = {

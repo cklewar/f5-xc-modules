168f57def0dcbbf9efe6b23be638f009433458ae
cklewar <cklewar@t-online.de>
2022-11-26 23:06:41 +0100

    v35

--- f5xc/api-credential/main.tf
+++ f5xc/api-credential/main.tf
@@ -1,15 +1,3 @@
-/*resource "volterra_api_credential" "credential" {
-  name                    = var.f5xc_api_credentials_name
-  api_credential_type     = var.f5xc_api_credential_type
-  api_credential_password = var.f5xc_api_credential_type == var.f5xc_api_credential_type_api_certificate && var.f5xc_api_credential_password != "" ? var.f5xc_api_credential_password : null
-  virtual_k8s_namespace   = var.f5xc_api_credential_type == var.f5xc_api_credential_type_kube_config && var.f5xc_virtual_k8s_namespace != "" ? var.f5xc_virtual_k8s_namespace : null
-  virtual_k8s_name        = var.f5xc_api_credential_type == var.f5xc_api_credential_type_kube_config && var.f5xc_virtual_k8s_name != "" ? var.f5xc_virtual_k8s_name : null
-  expiry_days             = var.f5xc_api_credential_expiry_days
-  lifecycle {
-    ignore_changes = [name]
-  }
-}*/
-
 resource "null_resource" "apply_credential" {
   provisioner "local-exec" {
     command     = "${path.module}/scripts/script.py post ${var.f5xc_api_url} ${var.f5xc_api_token} ${var.f5xc_tenant} -n ${var.f5xc_api_credentials_name} -v ${ var.f5xc_virtual_k8s_name} -c ${var.f5xc_api_credential_type_kube_config}"

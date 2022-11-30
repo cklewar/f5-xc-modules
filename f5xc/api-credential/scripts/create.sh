2a57d30cf392f1bba00479a675026e945b6f10b1
cklewar <c.cklewar@f5.com>
2022-11-30 22:35:17 +0100

    update api-creds module

--- f5xc/api-credential/scripts/create.sh
+++ f5xc/api-credential/scripts/create.sh
@@ -7,5 +7,6 @@ python3 -m venv "$PWD"/modules/f5xc/api-credential/venv
 source "$PWD"/$MODULES_PATH/venv/bin/activate
 python3 -m pip install install --upgrade pip
 python3 -m pip install -r "$PWD/$MODULES_PATH/scripts/requirements.txt"
+echo "python3 $MODULES_PATH/scripts/script.py post $api_url $api_token $tenant -n $api_credentials_name -v $virtual_k8s_name -c $api_credential_type_kube_config"
 python3 $MODULES_PATH/scripts/script.py post $api_url $api_token $tenant -n $api_credentials_name -v $virtual_k8s_name -c $api_credential_type_kube_config
-# python3 "$PWD/$MODULES_PATH/scripts/script.py post \"$api_url\" \"$api_token\" \"$tenant\" -n \"$api_credentials_name\" -v \"$virtual_k8s_name\" -c \"$api_credential_type_kube_config\""
+

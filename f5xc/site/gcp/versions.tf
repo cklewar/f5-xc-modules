86c69048227ec0d6e0d557c691d33fde0e119600
cklewar <cklewar@f5.com>
2022-12-14 16:18:23 +0100

    0.11.16 v4

--- f5xc/site/gcp/versions.tf
+++ f5xc/site/gcp/versions.tf
@@ -7,6 +7,11 @@ terraform {
       version = "= 0.11.16"
     }
 
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.39.0"
+    }
+
     local = ">= 2.2.3"
     null  = ">= 3.1.1"
   }

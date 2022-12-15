0d130abb6b00c9af50c3c77b363e76393e73a0c9
cklewar <cklewar@f5.com>
2022-12-15 14:45:04 +0100

    0.11.16 v18

--- f5xc/site/azure/data.tf
+++ f5xc/site/azure/data.tf
@@ -1,24 +1,24 @@
-data "azurerm_network_interface" "sli" {
+data "azurerm_network_interface" "master-0-sli" {
   depends_on          = [module.site_wait_for_online]
   name                = "master-0-sli"
   resource_group_name = volterra_azure_vnet_site.site.resource_group
 
 }
 
-data "azurerm_network_interface" "slo" {
+data "azurerm_network_interface" "master-0-slo" {
   depends_on          = [module.site_wait_for_online]
   name                = "master-0-slo"
   resource_group_name = volterra_azure_vnet_site.site.resource_group
 
 }
 
-data "azurerm_public_ip" "pib" {
+data "azurerm_public_ip" "master-0-pib" {
   depends_on          = [module.site_wait_for_online]
   name                = "master-0-public-ip"
   resource_group_name = volterra_azure_vnet_site.site.resource_group
 }
 
-data "azurerm_network_interface" "sli" {
+data "azurerm_network_interface" "master-1-sli" {
   depends_on          = [module.site_wait_for_online]
   count               = length(var.f5xc_azure_az_nodes) >= 2 ? 1 : 0
   name                = "master-1-sli"
@@ -26,7 +26,7 @@ data "azurerm_network_interface" "sli" {
 
 }
 
-data "azurerm_network_interface" "slo" {
+data "azurerm_network_interface" "master-1-slo" {
   depends_on          = [module.site_wait_for_online]
   count               = length(var.f5xc_azure_az_nodes) >= 2 ? 1 : 0
   name                = "master-1-slo"
@@ -34,14 +34,14 @@ data "azurerm_network_interface" "slo" {
 
 }
 
-data "azurerm_public_ip" "pib" {
+data "azurerm_public_ip" "master-1-pib" {
   depends_on          = [module.site_wait_for_online]
   count               = length(var.f5xc_azure_az_nodes) >= 2 ? 1 : 0
   name                = "master-1-public-ip"
   resource_group_name = volterra_azure_vnet_site.site.resource_group
 }
 
-data "azurerm_network_interface" "sli" {
+data "azurerm_network_interface" "master-2-sli" {
   depends_on          = [module.site_wait_for_online]
   count               = length(var.f5xc_azure_az_nodes) >= 2 ? 1 : 0
   name                = "master-2-sli"
@@ -49,7 +49,7 @@ data "azurerm_network_interface" "sli" {
 
 }
 
-data "azurerm_network_interface" "slo" {
+data "azurerm_network_interface" "master-2-slo" {
   depends_on          = [module.site_wait_for_online]
   count               = length(var.f5xc_azure_az_nodes) >= 2 ? 1 : 0
   name                = "master-2-slo"
@@ -57,7 +57,7 @@ data "azurerm_network_interface" "slo" {
 
 }
 
-data "azurerm_public_ip" "pib" {
+data "azurerm_public_ip" "master-2-pib" {
   depends_on          = [module.site_wait_for_online]
   count               = length(var.f5xc_azure_az_nodes) >= 2 ? 1 : 0
   name                = "master-2-public-ip"

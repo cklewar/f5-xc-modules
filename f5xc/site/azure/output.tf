9517e5a96b7f7d19276dcf15c7e4923b5b6602de
cklewar <cklewar@f5.com>
2022-12-15 19:02:20 +0100

    v6

--- f5xc/site/azure/output.tf
+++ f5xc/site/azure/output.tf
@@ -9,21 +9,21 @@ output "vnet" {
     nodes          = {
       master-0 = {
         interfaces = {
-          sli_ip    = data.azurerm_network_interface.master-0-sli.private_ip_address
+          sli_ip    = var.f5xc_azure_ce_gw_type == var.f5xc_nic_type_multi_nic ? data.azurerm_network_interface.master-0-sli.private_ip_address : null
           slo_ip    = data.azurerm_network_interface.master-0-slo.private_ip_address
           public_ip = data.azurerm_public_ip.master-0-pib.ip_address
         }
       }
       master-1 = {
         interfaces = {
-          sli_ip    = data.azurerm_network_interface.master-1-sli.*.private_ip_address
+          sli_ip    = var.f5xc_azure_ce_gw_type == var.f5xc_nic_type_multi_nic ? data.azurerm_network_interface.master-1-sli.*.private_ip_address : null
           slo_ip    = data.azurerm_network_interface.master-1-slo.*.private_ip_address
           public_ip = data.azurerm_public_ip.master-1-pib.*.ip_address
         }
       }
       master-2 = {
         interfaces = {
-          sli_ip    = data.azurerm_network_interface.master-2-sli.*.private_ip_address
+          sli_ip    =  var.f5xc_azure_ce_gw_type == var.f5xc_nic_type_multi_nic ? data.azurerm_network_interface.master-2-sli.*.private_ip_address : null
           slo_ip    = data.azurerm_network_interface.master-2-slo.*.private_ip_address
           public_ip = data.azurerm_public_ip.master-2-pib.*.ip_address
         }

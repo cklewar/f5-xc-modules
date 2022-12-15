54f1cb3b2a8bdaf0f1028498bca268e99e9ac91f
cklewar <cklewar@f5.com>
2022-12-15 14:38:15 +0100

    0.11.16 v17

--- f5xc/site/azure/data.tf
+++ f5xc/site/azure/data.tf
@@ -17,3 +17,49 @@ data "azurerm_public_ip" "pib" {
   name                = "master-0-public-ip"
   resource_group_name = volterra_azure_vnet_site.site.resource_group
 }
+
+data "azurerm_network_interface" "sli" {
+  depends_on          = [module.site_wait_for_online]
+  count               = length(var.f5xc_azure_az_nodes) >= 2 ? 1 : 0
+  name                = "master-1-sli"
+  resource_group_name = volterra_azure_vnet_site.site.resource_group
+
+}
+
+data "azurerm_network_interface" "slo" {
+  depends_on          = [module.site_wait_for_online]
+  count               = length(var.f5xc_azure_az_nodes) >= 2 ? 1 : 0
+  name                = "master-1-slo"
+  resource_group_name = volterra_azure_vnet_site.site.resource_group
+
+}
+
+data "azurerm_public_ip" "pib" {
+  depends_on          = [module.site_wait_for_online]
+  count               = length(var.f5xc_azure_az_nodes) >= 2 ? 1 : 0
+  name                = "master-1-public-ip"
+  resource_group_name = volterra_azure_vnet_site.site.resource_group
+}
+
+data "azurerm_network_interface" "sli" {
+  depends_on          = [module.site_wait_for_online]
+  count               = length(var.f5xc_azure_az_nodes) >= 2 ? 1 : 0
+  name                = "master-2-sli"
+  resource_group_name = volterra_azure_vnet_site.site.resource_group
+
+}
+
+data "azurerm_network_interface" "slo" {
+  depends_on          = [module.site_wait_for_online]
+  count               = length(var.f5xc_azure_az_nodes) >= 2 ? 1 : 0
+  name                = "master-2-slo"
+  resource_group_name = volterra_azure_vnet_site.site.resource_group
+
+}
+
+data "azurerm_public_ip" "pib" {
+  depends_on          = [module.site_wait_for_online]
+  count               = length(var.f5xc_azure_az_nodes) >= 2 ? 1 : 0
+  name                = "master-2-public-ip"
+  resource_group_name = volterra_azure_vnet_site.site.resource_group
+}

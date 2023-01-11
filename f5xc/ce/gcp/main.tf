0864ebb324cfa1acb9f937b0fbabec7120d4d6b3
cklewar <cklewar@t-online.de>
2023-01-11 13:52:26 +0100

    0.11.16 v34

--- f5xc/ce/gcp/main.tf
+++ f5xc/ce/gcp/main.tf
@@ -7,27 +7,25 @@ module "network" {
   source                = "./network"
   gcp_region            = var.gcp_region
   network_name          = var.network_name
-  fabric_subnet_outside = var.fabric_subnet_public
+  fabric_subnet_outside = var.fabric_subnet_outside
   fabric_subnet_inside  = var.fabric_subnet_inside
 }
 
 module "config" {
-  source                 = "./config"
-  public_name            = var.public_name
-  instance_name          = var.instance_name
-  volterra_token         = volterra_token.site.id
-  cluster_labels         = local.cluster_labels
-  ssh_public_key         = var.ssh_public_key
-  f5xc_ce_gateway_type   = var.f5xc_ce_gateway_type
-  f5xc_cluster_latitude  = var.f5xc_cluster_latitude
-  f5xc_cluster_longitude = var.f5xc_cluster_longitude
+  source                     = "./config"
+  instance_name              = var.instance_name
+  volterra_token             = volterra_token.site.id
+  cluster_labels             = local.cluster_labels
+  ssh_public_key             = var.ssh_public_key
+  host_localhost_public_name = var.host_localhost_public_name
+  f5xc_ce_gateway_type       = var.f5xc_ce_gateway_type
+  f5xc_cluster_latitude      = var.f5xc_cluster_latitude
+  f5xc_cluster_longitude     = var.f5xc_cluster_longitude
 }
 
 module "node" {
   source                      = "./node"
-  user_data                   = module.config.ce["user_data"]
   machine_type                = var.machine_type
-  cluster_size                = var.cluster_size
   ssh_username                = var.ssh_username
   machine_image               = var.machine_image
   instance_name               = var.instance_name
@@ -39,6 +37,8 @@ module "node" {
   f5xc_api_url                = var.f5xc_api_url
   f5xc_api_token              = var.f5xc_api_token
   f5xc_namespace              = var.f5xc_namespace
+  f5xc_ce_user_data           = module.config.ce["user_data"]
+  f5xc_cluster_size           = var.f5xc_cluster_size
   f5xc_registration_retry     = var.f5xc_registration_retry
   f5xc_registration_wait_time = var.f5xc_registration_wait_time
 }

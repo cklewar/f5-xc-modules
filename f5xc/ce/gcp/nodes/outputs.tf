c1f66a154aa50ff1b45a4aa56d5d32e8eebeceea
cklewar <cklewar@t-online.de>
2023-01-11 15:16:10 +0100

    0.11.16 v49

--- f5xc/ce/gcp/nodes/outputs.tf
+++ f5xc/ce/gcp/nodes/outputs.tf
@@ -5,7 +5,7 @@ output "ce" {
       name        = google_compute_instance.instance.name
       tags        = google_compute_instance.instance.tags
       labels      = google_compute_instance.instance.labels
-      sli_ip      = google_compute_instance.instance.network_interface[1].network_ip
+      sli_ip      = var.f5xc_ce_gateway_type == var.f5xc_ce_gateway_type_ingress_egress ? google_compute_instance.instance.network_interface[1].network_ip : null
       slo_ip      = google_compute_instance.instance.network_interface[0].network_ip
       public_ip   = google_compute_instance.instance.network_interface[0].access_config[0].nat_ip
       instance_id = google_compute_instance.instance.instance_id

6dbd25fe52dc234d92985faa87d4a46210758dff
cklewar <cklewar@t-online.de>
2023-01-11 15:20:11 +0100

    0.11.16 v52

--- f5xc/ce/gcp/config/output.tf
+++ f5xc/ce/gcp/config/output.tf
@@ -1,7 +1,9 @@
 output "ce" {
   value = {
-    user_data     = local.cloud_init_master_config.config
-    gateway_type  = local.gateway_type
-    hosts_context = local.hosts_localhost.config
+    master-0 = {
+      user_data     = local.cloud_init_master_config.config
+      gateway_type  = local.gateway_type
+      hosts_context = local.hosts_localhost.config
+    }
   }
 }

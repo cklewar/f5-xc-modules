3bac64ce1e19df033a0636e1b793b823aea506d2
cklewar <cklewar@t-online.de>
2023-01-12 17:20:55 +0100

    0.11.16 v62

--- f5xc/api-credential/locals.tf
+++ f5xc/api-credential/locals.tf
@@ -1,6 +1,6 @@
 locals {
   credential_get_uri = format(var.f5xc_credential_get_uri, var.f5xc_namespace)
-  state_file         = "${path.module}/_out/state.json"
+  state_file         = "${path.module}/_out/${var.f5xc_api_credentials_name}/state.json"
   script_file        = abspath("${path.module}/scripts/script.py")
   venv_path          = abspath(path.module)
 }

47877e670f32f6ff2e80b7418f50c0db7af739bc
cklewar <cklewar@t-online.de>
2023-01-12 17:03:02 +0100

    0.11.16 v59

--- f5xc/api-credential/scripts/create.sh
+++ f5xc/api-credential/scripts/create.sh
@@ -5,8 +5,8 @@ MODULES_PATH="modules/f5xc/api-credential"
 echo "$PWD"
 python3 -m venv "$PWD"/modules/f5xc/api-credential/venv
 source "$PWD"/$MODULES_PATH/venv/bin/activate
-python3 -m pip -qqq --exists-action ignore --no-input --no-color install --progress-bar off --upgrade pip
-python3 -m pip -qqq --exists-action ignore --no-input --no-color install --progress-bar off -r "$PWD/$MODULES_PATH/scripts/requirements.txt"
+python3 -m pip -qqq --exists-action i --no-input --no-color install --progress-bar off --upgrade pip
+python3 -m pip -qqq --exists-action i --no-input --no-color install --progress-bar off -r "$PWD/$MODULES_PATH/scripts/requirements.txt"
 
 if [[ $api_credential_type = "KUBE_CONFIG" ]]
 then

a110409d23f75bcded92d3e28a10b0e61e669ade
Tommy McNeely <thomas.mcneely@davita.com>
2023-01-20 08:21:09 -0700

    fix: optional network output

--- f5xc/ce/gcp/outputs.tf
+++ f5xc/ce/gcp/outputs.tf
@@ -3,7 +3,7 @@ output "ce" {
     master-0 = {
       node    = module.node[0].ce["master-0"]
       config  = module.config[0].ce["master-0"]
-      network = module.network[0].ce["master-0"]
+      network = local.create_network ? module.network[0].ce["master-0"] : null
     }
   }
 }

b897d11bb77393328a878104e5057f35da296bb6
cklewar <cklewar@t-online.de>
2023-01-20 21:49:01 +0100

    0.11.18 v13

--- f5xc/ce/gcp/main.tf
+++ f5xc/ce/gcp/main.tf
@@ -45,6 +45,7 @@ module "node" {
   source                      = "./nodes"
   machine_type                = var.machine_type
   ssh_username                = var.ssh_username
+  has_public_ip               = var.has_public_ip
   instance_tags               = var.instance_tags
   machine_image               = var.machine_image
   instance_name               = var.instance_name
@@ -52,6 +53,7 @@ module "node" {
   slo_subnetwork              = var.fabric_subnet_outside != "" ? module.network[0].ce["master-${count.index}"]["slo_subnetwork"] : var.existing_fabric_subnet_outside
   ssh_public_key              = var.ssh_public_key
   machine_disk_size           = var.machine_disk_size
+  access_config_nat_ip        = var.access_config_nat_ip
   allow_stopping_for_update   = var.allow_stopping_for_update
   gcp_service_account_email   = var.gcp_service_account_email
   f5xc_tenant                 = var.f5xc_tenant
@@ -63,5 +65,4 @@ module "node" {
   f5xc_ce_gateway_type        = var.f5xc_ce_gateway_type
   f5xc_registration_retry     = var.f5xc_registration_retry
   f5xc_registration_wait_time = var.f5xc_registration_wait_time
-  use_public_ip               = var.use_public_ip
 }

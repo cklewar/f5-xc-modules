16ee8d60cfb0ee07c2ce30d997091e87201384ce
cklewar <cklewar@t-online.de>
2023-01-24 01:24:17 +0100

    0.11.18 v25

--- f5xc/site/update/variables.tf
+++ f5xc/site/update/variables.tf
@@ -23,12 +23,10 @@ variable "f5xc_api_p12_file" {
 
 variable "f5xc_api_token" {
   type      = string
-  sensitive = true
 }
 
 variable "f5xc_tenant" {
   type      = string
-  sensitive = true
 }
 
 variable "f5xc_namespace" {
@@ -61,4 +59,9 @@ variable "f5xc_site_name" {
 variable "f5xc_cluster_labels" {
   type    = map(string)
   default = {}
+}
+
+variable "is_sensitive" {
+  type    = bool
+  default = false
 }

d01da6bd914eddb164d4c8de3448c49abc36e243
cklewar <cklewar@t-online.de>
2023-01-26 16:29:16 +0100

    0.11.18 v46

--- f5xc/site/aws/tgw/outputs.tf
+++ f5xc/site/aws/tgw/outputs.tf
@@ -26,6 +26,48 @@ output "f5xc_aws_tgw" {
           }
         }
       }
+      master-1 = length(var.f5xc_aws_tgw_az_nodes) >= 2 ? {
+        id         = data.aws_instance.master-1.id
+        name       = data.aws_instance.master-1.tags["Name"]
+        interfaces = {
+          slo = {
+            id               = data.aws_network_interface.master-1-slo.*.id[0]
+            private_ip       = data.aws_network_interface.master-1-slo.*.private_ip[0]
+            private_dns_name = data.aws_network_interface.master-1-slo.*.private_dns_name[0]
+            public_ip        = data.aws_network_interface.master-1-slo.*.association[0][0].public_ip
+            public_dns_name  = data.aws_network_interface.master-1-slo.*.association[0][0].public_dns_name
+            subnet_id        = data.aws_network_interface.master-1-slo.*.subnet_id[0]
+          },
+          sli = {
+            id               = data.aws_network_interface.master-1-sli.*.id[0]
+            private_ip       = data.aws_network_interface.master-1-sli.*.private_ip[0]
+            private_dns_name = data.aws_network_interface.master-1-sli.*.private_dns_name[0]
+            subnet_id        = data.aws_network_interface.master-1-sli.*.subnet_id[0]
+            route_table_id   = data.aws_route_table.master-1-sli-rt.*.id[0]
+          }
+        }
+      } : null,
+      master-2 = length(var.f5xc_aws_tgw_az_nodes) >= 2 ? {
+        id         = data.aws_instance.master-2.id
+        name       = data.aws_instance.master-2.tags["Name"]
+        interfaces = {
+          slo = {
+            id               = data.aws_network_interface.master-2-slo.*.id[0]
+            private_ip       = data.aws_network_interface.master-2-slo.*.private_ip[0]
+            private_dns_name = data.aws_network_interface.master-2-slo.*.private_dns_name[0]
+            public_ip        = data.aws_network_interface.master-2-slo.*.association[0][0].public_ip
+            public_dns_name  = data.aws_network_interface.master-2-slo.*.association[0][0].public_dns_name
+            subnet_id        = data.aws_network_interface.master-2-slo.*.subnet_id
+          },
+          sli = {
+            id               = data.aws_network_interface.master-2-sli.*.id[0]
+            private_ip       = data.aws_network_interface.master-2-sli.*.private_ip[0]
+            private_dns_name = data.aws_network_interface.master-2-sli.*.private_dns_name[0]
+            subnet_id        = data.aws_network_interface.master-2-sli.*.subnet_id[0]
+            route_table_id   = data.aws_route_table.master-2-sli-rt.*.id[0]
+          }
+        }
+      } : null
     }
   }
 }

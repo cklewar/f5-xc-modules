4abe09480b8aa544de83f9ad5c4d4f9e442c0fe9
cklewar <cklewar@t-online.de>
2023-01-30 20:56:44 +0100

    aws_ce v10

--- f5xc/ce/aws/main.tf
+++ f5xc/ce/aws/main.tf
@@ -9,7 +9,7 @@ resource "volterra_token" "site" {
 
 module "config" {
   source               = "./config"
-  count                = length(local.f5xc_aws_vpc_az_nodes)
+  for_each             = {for idx, node in local.f5xc_aws_vpc_az_nodes : node=>node}
   owner_tag            = var.owner_tag
   public_name          = var.public_name
   public_address       = "192.168.2.2" # module.ce_network.balancer_ip
@@ -20,7 +20,7 @@ module "config" {
   cluster_latitude     = var.cluster_latitude
   cluster_longitude    = var.cluster_longitude
   ssh_public_key       = var.ssh_public_key
-  server_roles         = local.server_roles[count.index]
+  server_roles         = local.server_roles[each.key]
   f5xc_ce_gateway_type = var.f5xc_ce_gateway_type
 }
 

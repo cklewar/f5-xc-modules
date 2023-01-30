9a75183ac50c54a33597448765b7e1dc608c66a8
cklewar <cklewar@t-online.de>
2023-01-30 20:44:20 +0100

    aws_ce v9

--- f5xc/ce/aws/outputs.tf
+++ f5xc/ce/aws/outputs.tf
@@ -1,9 +1,9 @@
 output "ce" {
   value = {
     master-0 = {
-      node    = module.node[0].ce
+      # node    = module.node[0].ce
       config  = module.config[0].ce
-      network = local.create_network ? module.network[0].ce : null
+      # network = local.create_network ? module.network[0].ce : null
     }
   }
 }

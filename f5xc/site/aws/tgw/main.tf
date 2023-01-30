561a863a60c986c21348f2684f8441d10e82e1b1
Marcel Wiget <m.wiget@f5.com>
2023-01-30 13:07:13 +0100

    enable labels for tgw site

--- f5xc/site/aws/tgw/main.tf
+++ f5xc/site/aws/tgw/main.tf
@@ -203,13 +203,13 @@ resource "volterra_aws_tgw_site" "site" {
   }
 }
 
-/*resource "volterra_cloud_site_labels" "labels" {
+resource "volterra_cloud_site_labels" "labels" {
   name             = volterra_aws_tgw_site.site.name
-  site_type        = "aws_vpc_site"
+  site_type        = "aws_tgw_site"
   # need at least one label, otherwise site_type is ignored
-  labels           = merge({ "key" = "value" }, var.custom_tags)
+  labels           = merge({ "key" = "value" }, var.custom_tags, var.f5xc_aws_tgw_labels)
   ignore_on_delete = var.f5xc_cloud_site_labels_ignore_on_delete
-}*/
+}
 
 resource "volterra_tf_params_action" "aws_tgw_action" {
   site_name       = volterra_aws_tgw_site.site.name

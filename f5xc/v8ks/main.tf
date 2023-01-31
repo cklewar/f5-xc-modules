8606c0c8855bd6d27e874ee5ad0cc09063e0fe46
cklewar <cklewar@t-online.de>
2023-01-31 21:16:07 +0100

    0.11.18 v55

--- f5xc/v8ks/main.tf
+++ f5xc/v8ks/main.tf
@@ -13,15 +13,6 @@ resource "volterra_virtual_k8s" "vk8s" {
       tenant    = local.f5xc_tenant
     }
   }
-
-  /*dynamic "default_flavor_ref" {
-    for_each = var.f5xc_vk8s_default_flavor_name != "" ? [1] : []
-    content {
-      name      = var.f5xc_vk8s_default_flavor_name
-      tenant    = var.f5xc_tenant
-      namespace = var.f5xc_namespace
-    }
-  }*/
 }
 
 module "vk8s_wait_for_online" {
@@ -43,7 +34,7 @@ module "api_credential_kubeconfig" {
   f5xc_tenant                = local.f5xc_tenant
   f5xc_api_url               = var.f5xc_api_url
   f5xc_api_token             = local.f5xc_api_token
-  f5xc_namespace             = var.f5xc_namespace
+  f5xc_namespace             = var.f5xc_api_credential_namespace
   f5xc_virtual_k8s_name      = volterra_virtual_k8s.vk8s.name
   f5xc_virtual_k8s_namespace = volterra_virtual_k8s.vk8s.namespace
   f5xc_api_credential_type   = "KUBE_CONFIG"
--- f5xc/v8ks/variables.tf
+++ f5xc/v8ks/variables.tf
@@ -47,9 +47,8 @@ variable "f5xc_create_k8s_creds" {
   default = false
 }
 
-variable "f5xc_namespace" {
+variable "f5xc_api_credential_namespace" {
   type    = string
-  default = ""
 }
 
 variable "f5xc_k8s_credentials_name" {

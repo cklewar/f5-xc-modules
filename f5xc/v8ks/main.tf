89eb7daafe96b061b3ca07eb0ec2a53eee23e6c5
cklewar <cklewar@t-online.de>
2023-01-31 20:54:26 +0100

    0.11.18 v52

--- f5xc/v8ks/main.tf
+++ f5xc/v8ks/main.tf
@@ -36,16 +36,16 @@ module "vk8s_wait_for_online" {
 }
 
 module "api_credential_kubeconfig" {
-  depends_on                = [module.vk8s_wait_for_online]
-  count                     = var.f5xc_create_k8s_creds == true && var.f5xc_k8s_credentials_name != "" ? 1 : 0
-  source                    = "../api-credential"
-  is_sensitive              = var.is_sensitive
-  f5xc_tenant               = local.f5xc_tenant
-  f5xc_api_url              = var.f5xc_api_url
-  f5xc_api_token            = local.f5xc_api_token
-  f5xc_namespace            = var.f5xc_namespace
-  f5xc_virtual_k8s_name     = volterra_virtual_k8s.vk8s.name
+  depends_on                 = [module.vk8s_wait_for_online]
+  count                      = var.f5xc_create_k8s_creds == true && var.f5xc_k8s_credentials_name != "" ? 1 : 0
+  source                     = "../api-credential"
+  is_sensitive               = var.is_sensitive
+  f5xc_tenant                = local.f5xc_tenant
+  f5xc_api_url               = var.f5xc_api_url
+  f5xc_api_token             = local.f5xc_api_token
+  f5xc_namespace             = var.f5xc_namespace
+  f5xc_virtual_k8s_name      = volterra_virtual_k8s.vk8s.name
   f5xc_virtual_k8s_namespace = volterra_virtual_k8s.vk8s.namespace
-  f5xc_api_credential_type  = "KUBE_CONFIG"
-  f5xc_api_credentials_name = var.f5xc_k8s_credentials_name
+  f5xc_api_credential_type   = "KUBE_CONFIG"
+  f5xc_api_credentials_name  = var.f5xc_k8s_credentials_name
 }

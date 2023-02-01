907127d18b0a6b1e9da800477630b11aafe8db5c
cklewar <cklewar@t-online.de>
2023-02-01 20:44:29 +0100

    0.11.18 v68

--- f5xc/ce/aws/network/main.tf
+++ f5xc/ce/aws/network/main.tf
@@ -24,7 +24,7 @@ resource "aws_route" "route_ipv6" {
 }
 
 resource "aws_subnet" "slo" {
-  for_each          = contains(keys(var.f5xc_aws_vpc_az_nodes), "f5xc_aws_vpc_slo_subnet") ? var.f5xc_aws_vpc_az_nodes : []
+  for_each          = contains(keys(var.f5xc_aws_vpc_az_nodes), "f5xc_aws_vpc_slo_subnet") ? var.f5xc_aws_vpc_az_nodes : {}
   vpc_id            = var.aws_existing_vpc_id != "" ? var.aws_existing_vpc_id : aws_vpc.vpc[0].id
   cidr_block        = var.f5xc_aws_vpc_az_nodes[each.key]["f5xc_aws_vpc_slo_subnet"]
   availability_zone = var.f5xc_aws_vpc_az_nodes[each.key]["f5xc_aws_vpc_az_name"]

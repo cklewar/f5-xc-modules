d126738c5b0984b473115ece7d709e808cde7c42
cklewar <cklewar@t-online.de>
2023-02-06 23:17:44 +0100

    0.11.18 v93

--- f5xc/ce/aws/network/common/outputs.tf
+++ f5xc/ce/aws/network/common/outputs.tf
@@ -5,7 +5,7 @@ output "common" {
     sg_slo           = module.aws_security_group_slo.aws_security_group
     sg_sli           = module.aws_security_group_sli.aws_security_group
     existing_vpc     = var.aws_existing_vpc_id != "" ? data.aws_vpc.vpc : null
-    nat_gw_eip_id    = var.f5xc_ce_gateway_type == var.f5xc_ce_gateway_type_ingress_egress ? aws_eip.nat_gw_eip.id : null
+    nat_gw_eip_id    = var.f5xc_ce_gateway_type == var.f5xc_ce_gateway_type_ingress_egress ? aws_eip.nat_gw_eip[0].id : null
     instance_profile = aws_iam_instance_profile.instance_profile.name
   }
 }

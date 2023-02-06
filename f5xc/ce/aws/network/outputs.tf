79edc84256d4751cf23c9d2074244bc998b16162
cklewar <cklewar@t-online.de>
2023-02-06 09:56:09 +0100

    0.11.18 v75

--- f5xc/ce/aws/network/outputs.tf
+++ f5xc/ce/aws/network/outputs.tf
@@ -2,7 +2,7 @@ output "common" {
   value = {
     sg               = aws_security_group.sg
     vpc              = var.aws_existing_vpc_id == "" ? aws_vpc.vpc[0] : null
-    nlb              = aws_lb.nlb
+    # nlb              = aws_lb.nlb
     igw              = aws_internet_gateway.igw
     ngw              = var.f5xc_ce_gateway_type == var.f5xc_ce_gateway_type_ingress_egress ? aws_nat_gateway.ngw : null
     rt_sli           = var.f5xc_ce_gateway_type == var.f5xc_ce_gateway_type_ingress_egress ? aws_route_table.sli : null

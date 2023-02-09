9508c1f6c688599a2a4f027c2badfc56e4640d99
cklewar <cklewar@t-online.de>
2023-02-09 09:56:40 +0100

    0.11.18 v105

--- f5xc/ce/aws/network/common/data.tf
+++ f5xc/ce/aws/network/common/data.tf
@@ -1,4 +1,3 @@
 data "aws_vpc" "vpc" {
-  id   = var.aws_existing_vpc_id != "" ? var.aws_existing_vpc_id : aws_vpc.vpc[0].id
-  tags = local.common_tags
+  id = var.aws_existing_vpc_id != "" ? var.aws_existing_vpc_id : aws_vpc.vpc[0].id
 }

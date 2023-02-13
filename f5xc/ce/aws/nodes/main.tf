a6daf61f03272f4ea31d374ae07b23dd0e6bf06b
cklewar <cklewar@t-online.de>
2023-02-13 13:05:24 +0100

    0.11.18 v114

--- f5xc/ce/aws/nodes/main.tf
+++ f5xc/ce/aws/nodes/main.tf
@@ -60,7 +60,7 @@ module "site_wait_for_online" {
   f5xc_api_token = var.f5xc_api_token
   f5xc_api_url   = var.f5xc_api_url
   f5xc_namespace = var.f5xc_namespace
-  f5xc_site_name = aws_instance.instance.tags["Name"]
+  f5xc_site_name = var.cluster_name
   f5xc_tenant    = var.f5xc_tenant
   is_sensitive   = var.is_sensitive
 }

36f73bdcbe84d2780dbb003cab3c165778fffef1
cklewar <cklewar@t-online.de>
2023-02-13 10:15:19 +0100

    0.11.18 v109

--- f5xc/ce/aws/config/locals.tf
+++ f5xc/ce/aws/config/locals.tf
@@ -21,16 +21,16 @@ locals {
   vpm_config = yamlencode({
     "Vpm" : {
       # "Roles" : var.server_roles, # pool only
-      "SkipStages" : var.vp_manager_node_skip_stages # || var.vp_manager_pool_skip_stages,
+      # "SkipStages" : var.vp_manager_node_skip_stages # || var.vp_manager_pool_skip_stages,
       "DisableModules" : [],
-      "ClusterUid" : var.cluster_uid,
+      # "ClusterUid" : var.cluster_uid,
       "ClusterName" : var.cluster_name,
       "ClusterType" : var.cluster_type,
       "Token" : var.site_token,
       "InsideNIC" : var.public_nic,
-      "PrivateNIC" : var.f5xc_ce_gateway_type == var.f5xc_ce_gateway_type_ingress_egress ? var.private_nic : null,
-      "PrivateDefaultGw" : var.f5xc_ce_gateway_type == var.f5xc_ce_gateway_type_ingress_egress ? var.private_default_gw : null,
-      "PrivateVnPrefix" : var.f5xc_ce_gateway_type == var.f5xc_ce_gateway_type_ingress_egress ? var.private_vn_prefix : null,
+      "PrivateNIC" : var.f5xc_ce_gateway_type == var.f5xc_ce_gateway_type_ingress_egress ? var.private_nic : "",
+      "PrivateDefaultGw" : var.f5xc_ce_gateway_type == var.f5xc_ce_gateway_type_ingress_egress ? var.private_default_gw : "",
+      "PrivateVnPrefix" : var.f5xc_ce_gateway_type == var.f5xc_ce_gateway_type_ingress_egress ? var.private_vn_prefix : "",
       "CustomerRoute" : var.customer_route,
       "Latitude" : var.cluster_latitude,
       "Longitude" : var.cluster_longitude,
@@ -39,17 +39,17 @@ locals {
       "Labels" : var.cluster_labels,
       "CertifiedHardwareEndpoint" : var.certified_hardware_endpoint,
     }
-    Workload : var.cluster_workload,
+    # Workload : var.cluster_workload,
     Kubernetes : {
       "CloudProvider" : "",
       # "EtcdClusterServers" : [] # pool only
       "EtcdUseTLS" : true # node only
       "Server" : var.public_name
-      "Images" : {
-        "Hyperkube" : var.container_images["Hyperkube"]
-        "CoreDNS" : var.container_images["CoreDNS"] # node only
-        "Etcd" : var.container_images["Etcd"] # node only
-      }
+      #"Images" : {
+      #  "Hyperkube" : var.container_images["Hyperkube"]
+      #  "CoreDNS" : var.container_images["CoreDNS"] # node only
+      #  "Etcd" : var.container_images["Etcd"] # node only
+      #}
     }
   })
 

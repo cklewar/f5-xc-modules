99367f2de04e7d2cf7a51c9cf1ebebda52bbcc79
cklewar <c.klewar@f5.com>
2023-03-18 03:43:55 +0100

    0.11.19 v44

--- aws/security_group/variables.tf
+++ aws/security_group/variables.tf
@@ -1,6 +1,5 @@
 variable "aws_security_group_name" {
-  type    = string
-  default = "test_group"
+  type = string
 }
 
 variable "description" {
@@ -9,8 +8,7 @@ variable "description" {
 }
 
 variable "aws_vpc_id" {
-  type    = string
-  default = "1"
+  type = string
 }
 
 variable "custom_tags" {
@@ -18,75 +16,6 @@ variable "custom_tags" {
   type        = map(string)
   default     = {}
 }
-
-/*variable "f5xc_ip_ranges_Americas_TCP" {
-  type    = list(string)
-  default = ["84.54.62.0/25", "185.94.142.0/25", "185.94.143.0/25", "159.60.190.0/24", "5.182.215.0/25", "84.54.61.0/25", "23.158.32.0/25",]
-}
-variable "f5xc_ip_ranges_Americas_UDP" {
-  type    = list(string)
-  default = ["23.158.32.0/25", "84.54.62.0/25", "185.94.142.0/25", "185.94.143.0/25", "159.60.190.0/24", "5.182.215.0/25", "84.54.61.0/25",]
-}
-variable "f5xc_ip_ranges_Europe_TCP" {
-  type    = list(string)
-  default = ["84.54.60.0/25", "185.56.154.0/25", "159.60.162.0/24", "159.60.188.0/24", "5.182.212.0/25", "5.182.214.0/25", "159.60.160.0/24", "5.182.213.0/25", "5.182.213.128/25",]
-}
-variable "f5xc_ip_ranges_Europe_UDP" {
-  type    = list(string)
-  default = ["5.182.212.0/25", "185.56.154.0/25", "159.60.160.0/24", "5.182.213.0/25", "5.182.213.128/25", "5.182.214.0/25", "84.54.60.0/25", "159.60.162.0/24", "159.60.188.0/24",]
-}
-variable "f5xc_ip_ranges_Asia_TCP" {
-  type    = list(string)
-  default = ["103.135.56.0/25", "103.135.56.128/25", "103.135.58.128/25", "159.60.189.0/24", "159.60.166.0/24", "103.135.57.0/25", "103.135.59.0/25", "103.135.58.0/25", "159.60.164.0/24",]
-}
-variable "f5xc_ip_ranges_Asia_UDP" {
-  type    = list(string)
-  default = ["103.135.57.0/25", "103.135.56.128/25", "103.135.59.0/25", "103.135.58.0/25", "159.60.166.0/24", "159.60.164.0/24", "103.135.56.0/25", "103.135.58.128/25", "159.60.189.0/24",]
-}
-
-locals {
-  f5xc_ip_ranges_americas = setunion(var.f5xc_ip_ranges_Americas_TCP, var.f5xc_ip_ranges_Americas_UDP)
-  f5xc_ip_ranges_europe   = setunion(var.f5xc_ip_ranges_Europe_TCP, var.f5xc_ip_ranges_Europe_UDP)
-  f5xc_ip_ranges_asia     = setunion(var.f5xc_ip_ranges_Asia_TCP, var.f5xc_ip_ranges_Asia_UDP)
-  f5xc_ip_ranges_all      = setunion(var.f5xc_ip_ranges_Americas_TCP, var.f5xc_ip_ranges_Americas_UDP, var.f5xc_ip_ranges_Europe_TCP, var.f5xc_ip_ranges_Europe_UDP, var.f5xc_ip_ranges_Asia_TCP, var.f5xc_ip_ranges_Asia_UDP)
-}
-
-variable "f5xc_ce_egress_ip_ranges" {
-  type        = list(string)
-  description = "Egress IP ranges for F5 XC CE"
-  default     = [
-    "20.33.0.0/16",
-    "74.125.0.0/16",
-    "18.64.0.0/10",
-    "52.223.128.0/18",
-    "20.152.0.0/15",
-    "13.107.238.0/24",
-    "142.250.0.0/15",
-    "20.34.0.0/15",
-    "52.192.0.0/12",
-    "52.208.0.0/13",
-    "52.223.0.0/17",
-    "18.32.0.0/11",
-    "3.208.0.0/12",
-    "13.107.237.0/24",
-    "20.36.0.0/14",
-    "52.222.0.0/16",
-    "52.220.0.0/15",
-    "3.0.0.0/9",
-    "100.64.0.0/10",
-    "54.88.0.0/16",
-    "52.216.0.0/14",
-    "108.177.0.0/17",
-    "20.40.0.0/13",
-    "54.64.0.0/11",
-    "172.253.0.0/16",
-    "20.64.0.0/10",
-    "20.128.0.0/16",
-    "172.217.0.0/16",
-    "173.194.0.0/16",
-    "20.150.0.0/15"
-  ]
-}*/
 
 variable "security_group_rules_egress" {
   type = list(object({

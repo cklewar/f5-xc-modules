84fdce5b7d6f52e0abf5a6256060f57628b124bc
cklewar <c.klewar@f5.com>
2023-03-22 17:23:25 +0100

    0.11.20 v14

--- f5xc/ce/aws/network/common/locals.tf
+++ f5xc/ce/aws/network/common/locals.tf
@@ -1,6 +1,6 @@
 locals {
-  sg_slo_secure_ce_id          = var.f5xc_is_secure_cloud_ce ? module.aws_security_group_slo_secure_ce[0].aws_security_group["id"] : null
-  sg_slo_secure_ce_extended_id = var.f5xc_is_secure_cloud_ce ? module.aws_security_group_slo_secure_ce_extended[0].aws_security_group["id"] : null
+  sg_slo_secure_ce_id          = var.f5xc_is_secure_cloud_ce ? length(module.aws_security_group_slo_secure_ce) > 0 ? module.aws_security_group_slo_secure_ce[0].aws_security_group["id"] : null : null
+  sg_slo_secure_ce_extended_id = var.f5xc_is_secure_cloud_ce ? length(module.aws_security_group_slo_secure_ce_extended) > 0 ? module.aws_security_group_slo_secure_ce_extended[0].aws_security_group["id"] : null : null
   sg_sli_id                    = var.is_multi_nic ? module.aws_security_group_sli[0].aws_security_group["id"] : null
   sg_sli_secure_ce_id          = var.is_multi_nic && var.f5xc_is_secure_cloud_ce ? module.aws_security_group_sli_secure_ce[0].aws_security_group["id"] : null
 }

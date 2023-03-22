180a017f08328781e8dfb448314b6d7c593a7321
cklewar <c.klewar@f5.com>
2023-03-22 19:41:26 +0100

    0.11.20 v18

--- f5xc/ce/gcp/nodes/main.tf
+++ f5xc/ce/gcp/nodes/main.tf
@@ -63,7 +63,7 @@ module "site_wait_for_online" {
   f5xc_api_token = var.f5xc_api_token
   f5xc_api_url   = var.f5xc_api_url
   f5xc_namespace = var.f5xc_namespace
-  f5xc_site_name = google_compute_instance.instance.name
+  f5xc_site_name = var.instance_name
   f5xc_tenant    = var.f5xc_tenant
   is_sensitive   = var.is_sensitive
 }

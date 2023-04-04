ac52800f2d01a9a4f6755c8b1d30962728d692cc
cklewar <c.klewar@f5.com>
2023-04-04 22:21:36 +0200

    0.11.20 v71

--- f5xc/nfv/aws/outputs.tf
+++ f5xc/nfv/aws/outputs.tf
@@ -6,6 +6,7 @@ output "nfv" {
     virtual_server_ip        = local.nfv_virtual_server_ip
     disable_ssh_access       = volterra_nfv_service.nfv.disable_ssh_access
     disable_https_management = volterra_nfv_service.nfv.disable_https_management
+    pan_commands             = var.f5xc_nfv_type == var.f5xc_nfv_type_palo_alto_fw_service ? jsondecode(data.http.pan_commands.*.response_body[0])["spec"]["commands"] : null
     nodes                    = {
       instances             = var.f5xc_nfv_type == var.f5xc_nfv_type_f5_big_ip_aws_service ? {for node in data.aws_instance.nfv_bigip : node.tags["Name"] => node} : null
       instances             = var.f5xc_nfv_type == var.f5xc_nfv_type_palo_alto_fw_service ? {for node in data.aws_instance.nfv_pan : node.tags["Name"] => node} : null
@@ -15,7 +16,6 @@ output "nfv" {
       internal_interface_ip = var.f5xc_nfv_type == var.f5xc_nfv_type_f5_big_ip_aws_service ? {
         for interface in data.aws_network_interface.nfv_big_ip_internal_interface : interface.tags["ves.io/nfv-service-node-name"] =>interface
       } : null
-      commands              = var.f5xc_nfv_type == var.f5xc_nfv_type_palo_alto_fw_service ? jsondecode(data.http.pan_commands.*.response_body[0])["spec"]["commands"] : null
     }
   }
 }

d11bc170942ea50e9894f51820cfc0f498e97952
cklewar <c.klewar@f5.com>
2023-04-05 15:18:08 +0200

    0.11.20 v74

--- f5xc/nfv/aws/data.tf
+++ f5xc/nfv/aws/data.tf
@@ -8,17 +8,22 @@ data "http" "nfv_virtual_server_ip" {
   }
 }
 
-data "aws_instance" "nfv_bigip" {
+data "aws_instance" "nfv_bigip_node1" {
   depends_on = [module.f5xc_nfv_wait_for_online]
-  for_each   = var.f5xc_nfv_type == var.f5xc_nfv_type_f5_big_ip_aws_service ? var.f5xc_aws_nfv_nodes : {}
+  for_each   = var.f5xc_nfv_type == var.f5xc_nfv_type_f5_big_ip_aws_service ? { keys(var.f5xc_aws_nfv_nodes)[0] = lookup(var.f5xc_aws_nfv_nodes, keys(var.f5xc_aws_nfv_nodes)[0]) } : {}
   filter {
     name   = "tag:ves-io-site-name"
     values = [var.f5xc_nfv_aws_tgw_site_params.name]
   }
 
   filter {
-    name   = "tag:ves-io-nfv-service-node-name" # "tag:ves.io/nfv-service-node-name"
+    name   = "tag:ves.io/nfv-service-node-name"
     values = [each.key]
+  }
+
+  filter {
+    name   = "tag:f5xc-tenant"
+    values = [var.f5xc_tenant]
   }
 
   filter {
@@ -31,6 +36,38 @@ data "aws_instance" "nfv_bigip" {
     values = ["bigip"]
   }
 }
+
+data "aws_instance" "nfv_bigip_node2" {
+  depends_on = [module.f5xc_nfv_wait_for_online]
+  for_each   = var.f5xc_nfv_type == var.f5xc_nfv_type_f5_big_ip_aws_service && length(var.f5xc_aws_nfv_nodes) > 1 ? {
+    keys(var.f5xc_aws_nfv_nodes)[1] = lookup(var.f5xc_aws_nfv_nodes, keys(var.f5xc_aws_nfv_nodes)[1])
+  } : {}
+  filter {
+    name   = "tag:ves-io-site-name"
+    values = [var.f5xc_nfv_aws_tgw_site_params.name]
+  }
+
+  filter {
+    name   = "tag:ves-io-nfv-service-node-name"
+    values = [each.key]
+  }
+
+  filter {
+    name   = "tag:f5xc-tenant"
+    values = [var.f5xc_tenant]
+  }
+
+  filter {
+    name   = "tag:ves-io/nfv-service"
+    values = [var.f5xc_nfv_name]
+  }
+
+  filter {
+    name   = "tag:ves-io/nfv-service-type"
+    values = ["bigip"]
+  }
+}
+
 
 data "aws_instance" "nfv_pan" {
   depends_on = [module.f5xc_nfv_wait_for_online]
@@ -56,9 +93,9 @@ data "aws_instance" "nfv_pan" {
   }
 }
 
-data "aws_network_interface" "nfv_big_ip_external_interface" {
+data "aws_network_interface" "nfv_big_ip_external_interface_node1" {
   depends_on = [module.f5xc_nfv_wait_for_online]
-  for_each   = var.f5xc_nfv_type == var.f5xc_nfv_type_f5_big_ip_aws_service ? var.f5xc_aws_nfv_nodes : {}
+  for_each   = var.f5xc_nfv_type == var.f5xc_nfv_type_f5_big_ip_aws_service ? { keys(var.f5xc_aws_nfv_nodes)[0] = lookup(var.f5xc_aws_nfv_nodes, keys(var.f5xc_aws_nfv_nodes)[0]) } : {}
   filter {
     name   = "tag:Name"
     values = ["BIGIP-External-Private-Interface-0"]
@@ -70,7 +107,7 @@ data "aws_network_interface" "nfv_big_ip_external_interface" {
   }
 
   filter {
-    name   = "tag:ves-io-nfv-service-node-name" # "tag:ves.io/nfv-service-node-name"
+    name   = "tag:ves.io/nfv-service-node-name"
     values = [each.key]
   }
 
@@ -85,9 +122,9 @@ data "aws_network_interface" "nfv_big_ip_external_interface" {
   }
 }
 
-data "aws_network_interface" "nfv_big_ip_internal_interface" {
+data "aws_network_interface" "nfv_big_ip_internal_interface_node1" {
   depends_on = [module.f5xc_nfv_wait_for_online]
-  for_each   = var.f5xc_nfv_type == var.f5xc_nfv_type_f5_big_ip_aws_service ? var.f5xc_aws_nfv_nodes : {}
+  for_each   = var.f5xc_nfv_type == var.f5xc_nfv_type_f5_big_ip_aws_service ? { keys(var.f5xc_aws_nfv_nodes)[0] = lookup(var.f5xc_aws_nfv_nodes, keys(var.f5xc_aws_nfv_nodes)[0]) } : {}
 
   filter {
     name   = "tag:Name"
@@ -100,7 +137,7 @@ data "aws_network_interface" "nfv_big_ip_internal_interface" {
   }
 
   filter {
-    name   = "tag:ves-io-nfv-service-node-name" # "tag:ves.io/nfv-service-node-name"
+    name   = "tag:ves.io/nfv-service-node-name"
     values = [each.key]
   }
 
--- f5xc/nfv/aws/outputs.tf
+++ f5xc/nfv/aws/outputs.tf
@@ -8,13 +8,14 @@ output "nfv" {
     disable_https_management = volterra_nfv_service.nfv.disable_https_management
     pan_commands             = var.f5xc_nfv_type == var.f5xc_nfv_type_palo_alto_fw_service ? jsondecode(data.http.pan_commands.*.response_body[0])["spec"]["commands"] : null
     nodes                    = {
-      instances             = var.f5xc_nfv_type == var.f5xc_nfv_type_f5_big_ip_aws_service ? {for node in data.aws_instance.nfv_bigip : node.tags["Name"] => node} : null
-      instances             = var.f5xc_nfv_type == var.f5xc_nfv_type_palo_alto_fw_service ? {for node in data.aws_instance.nfv_pan : node.tags["Name"] => node} : null
-      external_interface_ip = var.f5xc_nfv_type == var.f5xc_nfv_type_f5_big_ip_aws_service ? {
-        for interface in data.aws_network_interface.nfv_big_ip_external_interface : interface.tags["ves.io/nfv-service-node-name"] =>interface
+      node1                       = var.f5xc_nfv_type == var.f5xc_nfv_type_f5_big_ip_aws_service ? {for node in data.aws_instance.nfv_bigip_node1 : node.tags["Name"] => node} : null
+      node2                       = var.f5xc_nfv_type == var.f5xc_nfv_type_f5_big_ip_aws_service && length(var.f5xc_aws_nfv_nodes) > 1 ? {for node in data.aws_instance.nfv_bigip_node2 : node.tags["Name"] => node} : null
+      instances                   = var.f5xc_nfv_type == var.f5xc_nfv_type_palo_alto_fw_service ? {for node in data.aws_instance.nfv_pan : node.tags["Name"] => node} : null
+      node1_external_interface_ip = var.f5xc_nfv_type == var.f5xc_nfv_type_f5_big_ip_aws_service ? {
+        for interface in data.aws_network_interface.nfv_big_ip_external_interface_node1 : interface.tags["ves.io/nfv-service-node-name"] => interface
       } : null
-      internal_interface_ip = var.f5xc_nfv_type == var.f5xc_nfv_type_f5_big_ip_aws_service ? {
-        for interface in data.aws_network_interface.nfv_big_ip_internal_interface : interface.tags["ves.io/nfv-service-node-name"] =>interface
+      node1_internal_interface_ip = var.f5xc_nfv_type == var.f5xc_nfv_type_f5_big_ip_aws_service ? {
+        for interface in data.aws_network_interface.nfv_big_ip_internal_interface_node1 : interface.tags["ves.io/nfv-service-node-name"] => interface
       } : null
     }
   }

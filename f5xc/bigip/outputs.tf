08f43f4e207a47db10d742536130d830921f7672
cklewar <c.klewar@f5.com>
2023-04-06 14:11:46 +0200

    0.11.20 v77

--- f5xc/bigip/outputs.tf
+++ f5xc/bigip/outputs.tf
@@ -1,7 +1,6 @@
-output "auth_token" {
-  value = data.http.auth_token.response_body
-}
-
-output "bigip_waf_policy" {
-  value = data.http.bigip_waf_policy.response_body
+output "bigip" {
+  value = {
+    auth_token = data.http.auth_token.response_body
+    waf_policy = data.http.bigip_waf_policy.response_body
+  }
 }

0645407a07308417b751a430e99429f66b13274b
cklewar <c.klewar@f5.com>
2023-04-25 12:34:12 +0200

    0.11.21 v6

--- f5xc/nfv/aws/locals.tf
+++ f5xc/nfv/aws/locals.tf
@@ -1,4 +1,5 @@
 locals {
+  is_cluster            = length(var.f5xc_aws_nfv_nodes) > 1 ? true : false
   f5xc_tenant           = var.is_sensitive ? sensitive(var.f5xc_tenant) : var.f5xc_tenant
   f5xc_api_token        = var.is_sensitive ? sensitive(var.f5xc_api_token) : var.f5xc_api_token
   nfv_svc_get_uri       = format(var.f5xc_nfv_svc_get_uri, var.f5xc_namespace, var.f5xc_nfv_name)

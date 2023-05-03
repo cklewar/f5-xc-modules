4be6c52f1099a257afe09fb077fec5c702e6d0b7
cklewar <cklewar@t-online.de>
2023-05-04 00:51:45 +0200

    0.11.21 v32

--- f5xc/secure-mesh-site/outputs.tf
+++ f5xc/secure-mesh-site/outputs.tf
@@ -3,16 +3,16 @@ output "secure-mesh-site" {
     smg      = module.smg
     provider = {
       aws = length(module.aws) > 0 ? {
-        ce     = module.aws[0].ce
-        config = restapi_object.secure_mesh_site_aws
+        ce   = module.aws[0].ce
+        site = restapi_object.secure_mesh_site_aws
       } : null
       gcp = length(module.gcp) > 0 ? {
-        ce     = module.gcp[0].ce
-        config = restapi_object.secure_mesh_site_gcp
+        ce   = module.gcp[0].ce
+        site = restapi_object.secure_mesh_site_gcp
       } : null
       azure = length(module.azure) > 0 ? {
-        ce     = module.azure[0].ce
-        config = restapi_object.secure_mesh_site_azure
+        ce   = module.azure[0].ce
+        site = restapi_object.secure_mesh_site_azure
       } : null
       /*vmware = length(module.vmware) > 0 ? {
         ce     = module.vmware[0].ce

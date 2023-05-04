59e8b90022f7111f5b256fe57c5717db226ed3b9
cklewar <cklewar@t-online.de>
2023-05-04 12:09:56 +0200

    0.11.21 v34

--- f5xc/site/virtual/output.tf
+++ f5xc/site/virtual/output.tf
@@ -1,7 +1,10 @@
 output "virtual_site" {
   value = {
-    id        = volterra_virtual_site.virtual_site.id
-    name      = volterra_virtual_site.virtual_site.name
-    site_type = volterra_virtual_site.virtual_site.site_type
+    id            = volterra_virtual_site.virtual_site.id
+    name          = volterra_virtual_site.virtual_site.name
+    labels        = volterra_virtual_site.virtual_site.labels
+    site_type     = volterra_virtual_site.virtual_site.site_type
+    description   = volterra_virtual_site.virtual_site.description
+    site_selector = volterra_virtual_site.virtual_site.site_selector
   }
 }

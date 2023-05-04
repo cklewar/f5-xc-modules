44d7a617be1ba74f5473c12c1ba452a4fcb879f4
cklewar <cklewar@t-online.de>
2023-05-04 20:32:41 +0200

    v0.11.21 v48

--- f5xc/ce/gcp/network/common/main.tf
+++ f5xc/ce/gcp/network/common/main.tf
@@ -9,3 +9,10 @@ resource "google_compute_network" "sli_vpc_network" {
   auto_create_subnetworks         = var.auto_create_subnetworks
   delete_default_routes_on_create = var.f5xc_is_secure_cloud_ce ? true : false
 }
+
+/*resource "google_compute_network_peering" "peering1" {
+  for_each     = ""
+  name         = "peering1"
+  network      = google_compute_network.default.self_link
+  peer_network = google_compute_network.other.self_link
+}*/

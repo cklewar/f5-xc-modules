9e95ab4d72599017590a4ffa0beaf95d48065a96
cklewar <cklewar@t-online.de>
2023-05-04 15:04:18 +0200

    0.11.21 v38

--- f5xc/site-mesh-group/main.tf
+++ f5xc/site-mesh-group/main.tf
@@ -1,6 +1,6 @@
 module "virtual_site" {
   source                                = "../site/virtual"
-  count                                 = var.f5xc_create_virtual_site && var.f5xc_virtual_site_name != ""? 1 : 0
+  count                                 = var.f5xc_create_virtual_site && var.f5xc_virtual_site_name != "" ? 1 : 0
   f5xc_virtual_site_name                = var.f5xc_virtual_site_name
   f5xc_virtual_site_type                = var.f5xc_virtual_site_type
   f5xc_virtual_site_selector_expression = var.f5xc_virtual_site_selector_expression
@@ -30,8 +30,8 @@ resource "volterra_site_mesh_group" "site_mesh_group" {
     content {
       hub_mesh_group {
         name      = var.f5xc_site_site_hub_name
-        namespace = var.f5xc_namespace
         tenant    = var.f5xc_tenant
+        namespace = var.f5xc_namespace
       }
     }
   }
@@ -39,9 +39,9 @@ resource "volterra_site_mesh_group" "site_mesh_group" {
   dynamic "virtual_site" {
     for_each = var.f5xc_virtual_site_name != "" ? [1] : []
     content {
-      name      = var.f5xc_virtual_site_name
-      namespace = var.f5xc_namespace
+      name      = var.f5xc_create_virtual_site ? module.virtual_site.virtual_site["name"] : var.f5xc_virtual_site_name
       tenant    = var.f5xc_tenant
+      namespace = var.f5xc_namespace
     }
   }
 }
--- f5xc/site-mesh-group/output.tf
+++ f5xc/site-mesh-group/output.tf
@@ -5,6 +5,6 @@ output "site_mesh_group" {
     type         = volterra_site_mesh_group.site_mesh_group.type
     labels       = volterra_site_mesh_group.site_mesh_group.labels
     description  = volterra_site_mesh_group.site_mesh_group.description
-    virtual_site = var.f5xc_create_virtual_site ? module.virtual_site[0].virtual_site : volterra_site_mesh_group.site_mesh_group.virtual_site[0]
+    virtual_site = volterra_site_mesh_group.site_mesh_group.virtual_site[0]
   }
 }

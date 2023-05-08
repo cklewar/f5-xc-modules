09e8d863fc40d7afa5b1c024cc54b143107df8f8
cklewar <cklewar@t-online.de>
2023-05-09 00:19:27 +0200

    0.11.21 v68

--- f5xc/site/gcp/locals.tf
+++ f5xc/site/gcp/locals.tf
@@ -1,8 +1,5 @@
 locals {
-<<<<<<< HEAD
   f5xc_instance_names            = [for name in split(",", regex(local.f5xc_gcp_instance_name_pattern, volterra_tf_params_action.gcp_vpc_action.tf_output)[0]) : trim(name, "\"")]
-=======
->>>>>>> d94e7d1 (v0.11.21 v65)
   f5xc_gcp_instance_name_pattern = "instance_names\\s*=\\s*\\[\"(.*?)\"\\]"
   f5xc_aws_ce_os_version         = var.f5xc_gcp_ce_os_version != "" && var.f5xc_gcp_default_ce_os_version == false ? var.f5xc_gcp_ce_os_version : null
   f5xc_aws_ce_sw_version         = var.f5xc_gcp_ce_sw_version != "" && var.f5xc_gcp_default_ce_sw_version == false ? var.f5xc_gcp_ce_sw_version : null

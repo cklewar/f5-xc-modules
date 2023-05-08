79fe6a5310a59b95bd732da9c622c72672db8fe1
cklewar <cklewar@t-online.de>
2023-05-09 00:13:35 +0200

    0.11.21 v55

--- f5xc/site/gcp/locals.tf
+++ f5xc/site/gcp/locals.tf
@@ -1,8 +1,5 @@
 locals {
-  # f5xc_gcp_instance_name_pattern = format("instance_names = \\[\\n\\s*\"(%s-\\w*)\",\\n\\s*\\]", var.f5xc_gcp_site_name)
-  # f5xc_gcp_instance_name_pattern = format("instance_names\\s=\\s\\[\\n\\s\\s\"(%s-\\w*)\"", var.f5xc_gcp_site_name)
-  # f5xc_gcp_instance_name_pattern = format("instance_names\\s=\\s\\[\\n\\s\\s\"(%s-\\w*)\"", var.f5xc_gcp_site_name)
-  # f5xc_gcp_instance_name_pattern = "instance_names\\s=\\s\\[\\n\\s*\"(.*?)\",\\n\\s*\\]"
+  f5xc_instance_names            = [for name in split(",", regex(local.f5xc_gcp_instance_name_pattern, volterra_tf_params_action.gcp_vpc_action.tf_output)[0]) : trim(name, "\"")]
   f5xc_gcp_instance_name_pattern = "instance_names\\s*=\\s*\\[\"(.*?)\"\\]"
   f5xc_aws_ce_os_version         = var.f5xc_gcp_ce_os_version != "" && var.f5xc_gcp_default_ce_os_version == false ? var.f5xc_gcp_ce_os_version : null
   f5xc_aws_ce_sw_version         = var.f5xc_gcp_ce_sw_version != "" && var.f5xc_gcp_default_ce_sw_version == false ? var.f5xc_gcp_ce_sw_version : null
--- f5xc/site/gcp/output.tf
+++ f5xc/site/gcp/output.tf
@@ -1,13 +1,17 @@
 output "gcp_vpc" {
   value = {
-    id            = volterra_gcp_vpc_site.site.id
-    name          = volterra_gcp_vpc_site.site.name
-    region        = volterra_gcp_vpc_site.site.gcp_region
-    slo_ip        = data.google_compute_instance.instance.network_interface.0.network_ip
-    sli_ip        = var.f5xc_gcp_ce_gw_type == var.f5xc_nic_type_multi_nic ? data.google_compute_instance.instance.network_interface.1.network_ip : ""
-    params        = volterra_tf_params_action.gcp_vpc_action
-    public_ip     = data.google_compute_instance.instance.network_interface.0.access_config.0.nat_ip
-    instance_name = regex(local.f5xc_gcp_instance_name_pattern, volterra_tf_params_action.gcp_vpc_action.tf_output)[0]
-    instance_type = volterra_gcp_vpc_site.site.instance_type
+    vpc   = volterra_gcp_vpc_site.site
+    nodes = {
+      node0 = {
+        for index, name in local.f5xc_instance_names : name => data.google_compute_instance.node0[index] if data.google_compute_instance.node0[index].id != null
+      }
+      node1 = {
+        for index, name in local.f5xc_instance_names : name => data.google_compute_instance.node1[index] if data.google_compute_instance.node1[index].id != null
+      }
+      node2 = {
+        for index, name in local.f5xc_instance_names : name => data.google_compute_instance.node2[index] if data.google_compute_instance.node2[index].id != null
+      }
+    }
+    params = volterra_tf_params_action.gcp_vpc_action
   }
 }

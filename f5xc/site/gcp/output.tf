2195a0b5e10492d5ef0c8fb605bd47f48750132f
cklewar <cklewar@t-online.de>
2023-05-09 00:27:30 +0200

    0.11.21 v69

--- f5xc/site/gcp/output.tf
+++ f5xc/site/gcp/output.tf
@@ -5,12 +5,12 @@ output "gcp_vpc" {
       node0 = {
         for index, name in local.f5xc_instance_names : name => data.google_compute_instance.node0[index] if data.google_compute_instance.node0[index].id != null
       }
-      node1 = {
+      node1 = var.f5xc_gcp_node_number > 1 ? {
         for index, name in local.f5xc_instance_names : name => data.google_compute_instance.node1[index] if data.google_compute_instance.node1[index].id != null
-      }
-      node2 = {
+      } : null
+      node2 = var.f5xc_gcp_node_number > 1 ? {
         for index, name in local.f5xc_instance_names : name => data.google_compute_instance.node2[index] if data.google_compute_instance.node2[index].id != null
-      }
+      } : null
     }
     params = volterra_tf_params_action.gcp_vpc_action
   }

0ab8886e788a29511a04c5bab065cdda3eedc92a
cklewar <cklewar@t-online.de>
2023-05-09 14:46:13 +0200

    0.11.21 v71

--- f5xc/site/gcp/data.tf
+++ f5xc/site/gcp/data.tf
@@ -1,6 +1,6 @@
 data "google_compute_instance" "node0" {
   depends_on = [module.site_wait_for_online]
-  count      = length(local.f5xc_instance_names)
+  count      = var.f5xc_gcp_node_number
   name       = local.f5xc_instance_names[count.index]
   zone       = element(var.f5xc_gcp_zone_names, 0)
 }

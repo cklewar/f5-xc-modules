8afcb26a351268bbe3e9da8cc3b95e41158eade7
cklewar <cklewar@t-online.de>
2023-05-10 15:47:37 +0200

    0.11.21 v78

--- f5xc/enhanced-fw-policy/variables.tf
+++ f5xc/enhanced-fw-policy/variables.tf
@@ -8,27 +8,27 @@ variable "f5xc_enhanced_fw_policy_name" {
 
 variable "f5xc_enhanced_fw_policy_rules" {
   type = list(object({
-    allow      = bool
-    all_source = bool
+    allow      = optional(bool)
+    all_source = optional(bool)
     metadata   = object({
       name = string
     })
-    source_aws_vpc_ids = object({
+    source_aws_vpc_ids = optional(object({
       vpc_id = list(string)
-    })
-    source_prefix_list = object({
+    }))
+    source_prefix_list = optional(object({
       prefixes = list(string)
-    })
-    destination_prefix_list = object({
+    }))
+    destination_prefix_list = optional(object({
       prefixes = list(string)
-    })
-    applications = object({
+    }))
+    applications = optional(object({
       applications = list(string)
-    })
-    insert_service = object({
+    }))
+    insert_service = optional(object({
       nfv_service = object({
         name = string
       })
-    })
+    }))
   }))
 }

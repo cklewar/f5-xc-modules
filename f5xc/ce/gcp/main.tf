4c4329a1856a2702a1faa8ec7efdffb30ff79d64
cklewar <cklewar@t-online.de>
2023-05-10 17:59:16 +0200

    v0.11.21 v81

--- f5xc/ce/gcp/main.tf
+++ f5xc/ce/gcp/main.tf
@@ -86,22 +86,3 @@ module "node" {
   f5xc_registration_retry                          = var.f5xc_registration_retry
   f5xc_registration_wait_time                      = var.f5xc_registration_wait_time
 }
-
-/*module "timeout" {
-  source         = "../../../utils/timeout"
-  depend_on      = [module.node.ce]
-  create_timeout = "1m"
-  delete_timeout = "1m"
-}*/
-
-module "site_wait_for_online" {
-  # depends_on     = [module.timeout]
-  depends_on     = [module.node.ce]
-  source         = "../../status/site"
-  f5xc_api_token = var.f5xc_api_token
-  f5xc_api_url   = var.f5xc_api_url
-  f5xc_namespace = var.f5xc_namespace
-  f5xc_site_name = var.f5xc_cluster_name
-  f5xc_tenant    = var.f5xc_tenant
-  is_sensitive   = var.is_sensitive
-}

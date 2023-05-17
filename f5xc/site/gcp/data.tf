8e5f423746c57a4a575eeee9c785c4625c490604
cklewar <cklewar@t-online.de>
2023-05-17 15:09:11 +0200

    0.11.21 v101

--- f5xc/site/gcp/data.tf
+++ f5xc/site/gcp/data.tf
@@ -1,20 +1,19 @@
 data "google_compute_instance" "node0" {
   depends_on = [module.site_wait_for_online]
-  count      = var.f5xc_gcp_node_number
   name       = local.f5xc_instance_names[count.index]
   zone       = element(var.f5xc_gcp_zone_names, 0)
 }
 
 data "google_compute_instance" "node1" {
   depends_on = [module.site_wait_for_online]
-  count      = var.f5xc_gcp_node_number > 1 ? var.f5xc_gcp_node_number : 0
+  count      = var.f5xc_gcp_node_number > 1 ? 1 : 0
   name       = local.f5xc_instance_names[count.index]
   zone       = element(var.f5xc_gcp_zone_names, 1)
 }
 
 data "google_compute_instance" "node2" {
   depends_on = [module.site_wait_for_online]
-  count      = var.f5xc_gcp_node_number > 1 ? var.f5xc_gcp_node_number : 0
+  count      = var.f5xc_gcp_node_number > 1 ? 1 : 0
   name       = local.f5xc_instance_names[count.index]
   zone       = element(var.f5xc_gcp_zone_names, 2)
 }

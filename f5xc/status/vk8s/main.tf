c2f3ce504dbf921f78f9349f0d230201932c16ab
cklewar <cklewar@t-online.de>
2023-05-26 16:41:24 +0200

    0.11.22 v7

--- f5xc/status/vk8s/main.tf
+++ f5xc/status/vk8s/main.tf
@@ -5,7 +5,7 @@ resource "null_resource" "check_vk8s_status" {
   }
 
   provisioner "local-exec" {
-    command     = format("%s/scripts/check.sh %s %s %s %s", path.module, local.site_get_url, local.f5xc_api_token, local.f5xc_tenant, var.f5xc_max_timeout, local.site_get_url_filter, var.check_type_token)
+    command     = format("%s/scripts/check.sh %s %s %s %s %s %s", path.module, local.site_get_url, local.f5xc_api_token, local.f5xc_tenant, var.f5xc_max_timeout, local.site_get_url_filter, var.check_type_token)
     interpreter = ["/usr/bin/env", "bash", "-c"]
   }
 }
@@ -17,7 +17,7 @@ resource "null_resource" "check_vk8s_status" {
   }
 
   provisioner "local-exec" {
-    command     = format("%s/scripts/check.sh %s %s %s %s", path.module, local.site_get_url, var.f5xc_api_p12_cert, local.f5xc_tenant, var.f5xc_max_timeout, local.site_get_url_filter, var.check_type_token, var.f5xc_api_p12_cert_password)
+    command     = format("%s/scripts/check.sh %s %s %s %s %s %s %s", path.module, local.site_get_url, var.f5xc_api_p12_cert, local.f5xc_tenant, var.f5xc_max_timeout, local.site_get_url_filter, var.check_type_token, var.f5xc_api_p12_cert_password)
     interpreter = ["/usr/bin/env", "bash", "-c"]
   }
 }

5cd6b80c69239860c75be9f15977597b8f09973a
cklewar <cklewar@t-online.de>
2023-06-01 01:29:56 +0200

    Issue tracker 848

--- gcp/network/output.tf
+++ gcp/network/output.tf
@@ -2,6 +2,7 @@ output "vpc_network" {
   value = {
     "id"           = google_compute_network.vpc_network.id
     "name"         = google_compute_network.vpc_network.name
+    "self_link"    = google_compute_network.vpc_network.self_link
     "gateway_ipv4" = google_compute_network.vpc_network.gateway_ipv4
   }
 }

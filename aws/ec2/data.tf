f13ef29c8610f87fff1b961c3bd827f231d70595
cklewar <cklewar@t-online.de>
2023-06-02 00:28:25 +0200

    Issue tracker 848

--- aws/ec2/data.tf
+++ aws/ec2/data.tf
@@ -1,9 +1,12 @@
-/*data "aws_network_interface" "interfaces" {
-  for_each = {for interface in aws_instance.instance.network_interface : interface.network_interface_id => interface}
-  id = each.value.network_interface_id
+/*data "aws_network_interface" "inside" {
+  id = aws_instance.instance.network_interface
 }*/
 
-data "aws_network_interface" "interfaces" {
+/*data "aws_network_interface" "outside" {
+  id = aws_instance.instance.network_interface[1]
+}*
+
+/*data "aws_network_interface" "interfaces" {
   count = length(aws_instance.instance.network_interface)
   id    = tolist(aws_instance.instance.network_interface)[count.index].network_interface_id
-}
+}*/

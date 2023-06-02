d9320e2e3c762cdeb4bccf88e51d44c124631ef1
cklewar <cklewar@t-online.de>
2023-06-02 13:14:29 +0200

    Issue tracker 848

--- azure/linux_virtual_machine/output.tf
+++ azure/linux_virtual_machine/output.tf
@@ -4,7 +4,6 @@ output "virtual_machine" {
     name                  = azurerm_linux_virtual_machine.vm.name
     public_ip             = azurerm_linux_virtual_machine.vm.public_ip_address
     private_ip            = azurerm_linux_virtual_machine.vm.private_ip_address
-    interfaces            = [for interface in azurerm_network_interface.network_interface : interface]
     custom_data           = azurerm_linux_virtual_machine.vm.custom_data
     admin_ssh_key         = azurerm_linux_virtual_machine.vm.admin_ssh_key
     admin_username        = azurerm_linux_virtual_machine.vm.admin_username

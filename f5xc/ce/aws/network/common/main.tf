28a9fca55bc9579fbcba2602d155baab1d7665f2
cklewar <cklewar@t-online.de>
2023-06-16 13:04:17 +0200

    0.11.22 v2

--- f5xc/ce/aws/network/common/main.tf
+++ f5xc/ce/aws/network/common/main.tf
@@ -7,55 +7,56 @@ resource "aws_vpc" "vpc" {
 }
 
 module "aws_security_group_slo" {
-  source                      = "../../../../../aws/security_group"
-  aws_vpc_id                  = var.create_new_aws_vpc == false && var.aws_existing_vpc_id != "" ? var.aws_existing_vpc_id : aws_vpc.vpc[0].id
-  custom_tags                 = var.common_tags
-  description                 = "F5 XC CLOUD CE SLO SG"
-  aws_security_group_name     = format("%s-sg-slo", var.f5xc_cluster_name)
+  source                       = "../../../../../aws/security_group"
+  count                        = var.aws_security_group_rules_slo_egress != null ? 1 : 0
+  aws_vpc_id                   = var.create_new_aws_vpc == false && var.aws_existing_vpc_id != "" ? var.aws_existing_vpc_id : aws_vpc.vpc[0].id
+  custom_tags                  = var.common_tags
+  description                  = "F5 XC CLOUD CE SLO SG"
+  aws_security_group_name      = format("%s-sg-slo", var.f5xc_cluster_name)
   security_group_rules_egress  = var.aws_security_group_rules_slo_egress
   security_group_rules_ingress = var.aws_security_group_rules_slo_ingress
 }
 
 module "aws_security_group_sli" {
-  source                      = "../../../../../aws/security_group"
-  count                       = var.is_multi_nic ? 1 : 0
-  aws_vpc_id                  = var.aws_existing_vpc_id != "" ? var.aws_existing_vpc_id : aws_vpc.vpc[0].id
-  custom_tags                 = var.common_tags
-  description                 = "F5 XC CLOUD CE SLI SG"
-  aws_security_group_name     = format("%s-sg-sli", var.f5xc_cluster_name)
+  source                       = "../../../../../aws/security_group"
+  count                        = var.is_multi_nic ? 1 : 0
+  aws_vpc_id                   = var.aws_existing_vpc_id != "" ? var.aws_existing_vpc_id : aws_vpc.vpc[0].id
+  custom_tags                  = var.common_tags
+  description                  = "F5 XC CLOUD CE SLI SG"
+  aws_security_group_name      = format("%s-sg-sli", var.f5xc_cluster_name)
   security_group_rules_egress  = var.aws_security_group_rules_sli_egress
   security_group_rules_ingress = var.aws_security_group_rules_sli_ingress
 }
 
 module "aws_security_group_slo_secure_ce" {
-  source                      = "../../../../../aws/security_group"
-  count                       = var.f5xc_is_secure_cloud_ce || var.f5xc_ce_slo_enable_secure_sg ? 1 : 0
-  aws_vpc_id                  = var.create_new_aws_vpc == false && var.aws_existing_vpc_id != "" ? var.aws_existing_vpc_id : aws_vpc.vpc[0].id
-  custom_tags                 = var.common_tags
-  description                 = "F5 XC SECURE CLOUD CE SLO SG"
-  aws_security_group_name     = format("%s-sg-slo-secure-ce", var.f5xc_cluster_name)
+  source                       = "../../../../../aws/security_group"
+  count                        = var.f5xc_is_secure_cloud_ce || var.f5xc_ce_slo_enable_secure_sg ? 1 : 0
+  aws_vpc_id                   = var.create_new_aws_vpc == false && var.aws_existing_vpc_id != "" ? var.aws_existing_vpc_id : aws_vpc.vpc[0].id
+  custom_tags                  = var.common_tags
+  description                  = "F5 XC SECURE CLOUD CE SLO SG"
+  aws_security_group_name      = format("%s-sg-slo-secure-ce", var.f5xc_cluster_name)
   security_group_rules_egress  = var.aws_security_group_rules_slo_egress_secure_ce
   security_group_rules_ingress = var.aws_security_group_rules_slo_ingress_secure_ce
 }
 
 module "aws_security_group_slo_secure_ce_extended" {
-  source                      = "../../../../../aws/security_group"
-  count                       = var.f5xc_is_secure_cloud_ce || var.f5xc_ce_slo_enable_secure_sg ? 1 : 0
-  aws_vpc_id                  = var.create_new_aws_vpc == false && var.aws_existing_vpc_id != "" ? var.aws_existing_vpc_id : aws_vpc.vpc[0].id
-  custom_tags                 = var.common_tags
-  description                 = "F5 XC SECURE CLOUD CE SLO SG EXTENDED"
-  aws_security_group_name     = format("%s-sg-slo-secure-ce-extended", var.f5xc_cluster_name)
+  source                       = "../../../../../aws/security_group"
+  count                        = var.f5xc_is_secure_cloud_ce || var.f5xc_ce_slo_enable_secure_sg ? 1 : 0
+  aws_vpc_id                   = var.create_new_aws_vpc == false && var.aws_existing_vpc_id != "" ? var.aws_existing_vpc_id : aws_vpc.vpc[0].id
+  custom_tags                  = var.common_tags
+  description                  = "F5 XC SECURE CLOUD CE SLO SG EXTENDED"
+  aws_security_group_name      = format("%s-sg-slo-secure-ce-extended", var.f5xc_cluster_name)
   security_group_rules_egress  = var.aws_security_group_rules_slo_egress_secure_ce_extended
   security_group_rules_ingress = []
 }
 
 module "aws_security_group_sli_secure_ce" {
-  source                      = "../../../../../aws/security_group"
-  count                       = var.is_multi_nic && var.f5xc_is_secure_cloud_ce ? 1 : 0
-  aws_vpc_id                  = var.aws_existing_vpc_id != "" ? var.aws_existing_vpc_id : aws_vpc.vpc[0].id
-  custom_tags                 = var.common_tags
-  description                 = "F5 XC SECURE CLOUD CE SLI SG"
-  aws_security_group_name     = format("%s-sg-sli-secure-ce", var.f5xc_cluster_name)
+  source                       = "../../../../../aws/security_group"
+  count                        = var.is_multi_nic && var.f5xc_is_secure_cloud_ce ? 1 : 0
+  aws_vpc_id                   = var.aws_existing_vpc_id != "" ? var.aws_existing_vpc_id : aws_vpc.vpc[0].id
+  custom_tags                  = var.common_tags
+  description                  = "F5 XC SECURE CLOUD CE SLI SG"
+  aws_security_group_name      = format("%s-sg-sli-secure-ce", var.f5xc_cluster_name)
   security_group_rules_egress  = var.aws_security_group_rules_sli_egress_secure_ce
   security_group_rules_ingress = var.aws_security_group_rules_sli_ingress_secure_ce
 }

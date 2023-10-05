1cf4ffaa8c6c750e531749f2004968f7d19ab193
cklewar <cklewar@t-online.de>
2023-10-05 09:49:17 +0200

    remove unsupported argument

--- f5xc/site-mesh-group/main.tf
+++ f5xc/site-mesh-group/main.tf
@@ -4,6 +4,7 @@ module "virtual_site" {
   f5xc_virtual_site_name                = var.f5xc_virtual_site_name
   f5xc_virtual_site_type                = var.f5xc_virtual_site_type
   f5xc_virtual_site_selector_expression = var.f5xc_virtual_site_selector_expression
+
 }
 
 resource "volterra_site_mesh_group" "site_mesh_group" {

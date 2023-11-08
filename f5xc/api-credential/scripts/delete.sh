2112e31ca9ac7a6441e5ae6996f12dcf4bcbb5c1
cklewar <cklewar@t-online.de>
2023-11-08 23:23:53 +0100

    0.11.23 v9

--- f5xc/api-credential/scripts/delete.sh
+++ f5xc/api-credential/scripts/delete.sh
@@ -7,5 +7,12 @@ python3 -m venv "$MODULES_ROOT"/$MODULES_PATH//venv
 source "$MODULES_ROOT"/$MODULES_PATH/venv/bin/activate
 python3 -m pip -qqq --exists-action i --no-input --no-color install --progress-bar off --upgrade pip
 python3 -m pip -qqq --exists-action i --no-input --no-color install --progress-bar off -r "$MODULES_ROOT/$MODULES_PATH/scripts/requirements.txt"
-python3 "$MODULES_ROOT"/$MODULES_PATH/scripts/script.py delete $api_url $api_token $tenant $api_credentials_name "$MODULES_ROOT" "$storage" -r "$aws_region" -b "$s3_bucket" -k "$s3_key"
+
+if [[ $storage = "INTERNAL" ]]; then
+  python3 "$MODULES_ROOT"/$MODULES_PATH/scripts/script.py delete $api_url $api_token $tenant $api_credentials_name "$MODULES_ROOT" "$storage"
+
+else
+  python3 "$MODULES_ROOT"/$MODULES_PATH/scripts/script.py delete $api_url $api_token $tenant $api_credentials_name "$MODULES_ROOT" "$storage" -r "$aws_region" -b "$s3_bucket" -k "$s3_key"
+fi
+
 rm -Rf "$MODULES_ROOT"/$MODULES_PATH/venv

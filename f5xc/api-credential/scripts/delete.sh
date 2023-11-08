9bd49d26474cd50de38371b95232977602c684f3
cklewar <cklewar@t-online.de>
2023-11-08 23:29:54 +0100

    0.11.23 v11

--- f5xc/api-credential/scripts/delete.sh
+++ f5xc/api-credential/scripts/delete.sh
@@ -9,10 +9,10 @@ python3 -m pip -qqq --exists-action i --no-input --no-color install --progress-b
 python3 -m pip -qqq --exists-action i --no-input --no-color install --progress-bar off -r "$MODULES_ROOT/$MODULES_PATH/scripts/requirements.txt"
 
 if [[ $storage = "INTERNAL" ]]; then
-  python3 "$MODULES_ROOT"/$MODULES_PATH/scripts/script.py delete $api_url $api_token $tenant $api_credentials_name "$MODULES_ROOT" "$storage"
+  python3 "$MODULES_ROOT"/$MODULES_PATH/scripts/script.py delete "$api_url" "$api_token "$tenant" "$api_credentials_name" "$MODULES_ROOT" "$storage"
 
 else
-  python3 "$MODULES_ROOT"/$MODULES_PATH/scripts/script.py delete $api_url $api_token $tenant $api_credentials_name "$MODULES_ROOT" "$storage" -r "$aws_region" -b "$s3_bucket" -k "$s3_key"
+  python3 "$MODULES_ROOT"/$MODULES_PATH/scripts/script.py delete "$api_url" "$api_token" "$tenant" "$api_credentials_name" "$MODULES_ROOT" "$storage" -r "$aws_region" -b "$s3_bucket" -k "$s3_key"
 fi
 
 rm -Rf "$MODULES_ROOT"/$MODULES_PATH/venv

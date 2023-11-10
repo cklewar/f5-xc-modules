b7e5ac29076720e5d8007e894eab0bc691f5f227
cklewar <cklewar@t-online.de>
2023-11-10 14:40:42 +0100

    cleanup

--- f5xc/site/aws/tgw/outputs.tf
+++ f5xc/site/aws/tgw/outputs.tf
@@ -1,13 +1,13 @@
 output "f5xc_aws_tgw" {
   value = {
     tgw                 = data.aws_ec2_transit_gateway.tgw
+    params              = volterra_tf_params_action.aws_tgw_action
     tgw_vpc             = data.aws_vpc.tgw_vpc
     tgw_nlb             = var.f5xc_aws_tgw_enable_internet_vip ? data.aws_lb.tgw_nlb[0] : null
-    params              = volterra_tf_params_action.aws_tgw_action
     site_name           = volterra_aws_tgw_site.site.name
     namespace           = volterra_aws_tgw_site.site.namespace
-    workload_subnet_ids = length(data.aws_subnets.workload) > 0 ? data.aws_subnets.workload.ids : null
     nodes_counter       = length(var.f5xc_aws_tgw_az_nodes)
+    workload_subnet_ids = length(data.aws_subnets.workload) > 0 ? data.aws_subnets.workload.ids : null
     nodes               = {
       master-0 = {
         id         = data.aws_instance.master-0.id

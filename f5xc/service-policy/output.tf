4193d21b0dd5967357dc767845c199db6f993ec1
cklewar <cklewar@t-online.de>
2023-11-16 11:45:12 +0100

    0.11.23 v6

--- f5xc/service-policy/output.tf
+++ f5xc/service-policy/output.tf
@@ -1,15 +1,3 @@
 output "service_policy" {
   value = volterra_service_policy.service_policy
 }
-
-output "all" {
-  value = var.f5xc_service_policy
-}
-
-output "length" {
-  value = length(var.f5xc_service_policy.allow_list)
-}
-
-output "loop" {
-  value = {for k, v  in var.f5xc_service_policy.allow_list : k  => v}
-}

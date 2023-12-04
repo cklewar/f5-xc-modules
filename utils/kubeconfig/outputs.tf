d6020e1d4658c9fd7be50ee090eeeb9a012cd484
cklewar <cklewar@t-online.de>
2023-12-05 00:29:16 +0100

    cleanup

--- utils/kubeconfig/outputs.tf
+++ utils/kubeconfig/outputs.tf
@@ -2,10 +2,6 @@ output "config" {
   value = data.http.kubeconfig.response_body
 }
 
-output "config_base64" {
-  value = base64encode(data.http.kubeconfig.response_body)
-}
-
 output "file" {
   value = local_file.kubeconfig
 }

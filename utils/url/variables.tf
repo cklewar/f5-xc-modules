bb3572638d40ed774e5c02b42c2334e2912cc418
c.klewar <c.klewar@f5.com>
2023-12-12 12:51:41 +0100

    add url gen module

---
+++ utils/url/variables.tf
@@ -0,0 +1,18 @@
+variable "f5xc_url" {
+  type = string
+}
+
+variable "contains_string" {
+  type    = string
+  default = "console"
+}
+
+variable "schema" {
+  type    = string
+  default = "https://"
+}
+
+variable "file_suffix" {
+  type    = string
+  default = ".api-creds.p12"
+}

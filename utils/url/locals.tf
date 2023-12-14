600cee228f3a57de1ee74fc8cac7f9a896f17524
c.klewar <c.klewar@f5.com>
2023-12-14 15:39:10 +0100

    adding coredns feature

--- utils/url/locals.tf
+++ utils/url/locals.tf
@@ -4,6 +4,7 @@ locals {
   base        = join(".", slice(local._tmp, 1, length(local._tmp)))
   cert        = contains(local._tmp, var.contains_string) ? "${local.url}${local.file_suffix}" : "${local.tenant}.staging${local.file_suffix}"
   tenant      = split(".", local.url)[0]
+  tenant_full = ""
   schema      = var.schema
   api_url     = "${local.schema}${local.url}/api"
   api_token   = "${local.environment}_api_token"

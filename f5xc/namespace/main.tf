20d3d3e02f82b3999173296ecacba7cf00584430
c.klewar <c.klewar@f5.com>
2024-01-11 02:22:33 +0100

    add 0s option

--- f5xc/namespace/main.tf
+++ f5xc/namespace/main.tf
@@ -3,10 +3,12 @@ resource "volterra_namespace" "namespace" {
 }
 
 resource "time_sleep" "wait_n_seconds" {
+  count           = var.f5xc_namespace_create_timeout == "0s" ? 1 : 0
   depends_on      = [volterra_namespace.namespace]
   create_duration = var.f5xc_namespace_create_timeout
 }
 
 resource "null_resource" "next" {
+  count      = var.f5xc_namespace_create_timeout == "0s" ? 1 : 0
   depends_on = [time_sleep.wait_n_seconds]
 }

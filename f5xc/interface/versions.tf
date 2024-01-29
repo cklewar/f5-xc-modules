6fad3f0718cdffb220f6653768465f73e6184b84
c.klewar <c.klewar@f5.com>
2024-01-29 15:26:54 +0100

    init 0.11.30

--- f5xc/interface/versions.tf
+++ f5xc/interface/versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     volterra = {
       source  = "volterraedge/volterra"
-      version =  "= 0.11.24"
+      version =  "= 0.11.30"
     }
 
     http = {
--- f5xc/v8ks/versions.tf
+++ f5xc/v8ks/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     volterra = {
       source  = "volterraedge/volterra"
-      version =  "= 0.11.24"
+      version =  "= 0.11.30"
     }
 
     local = ">= 2.2.3"

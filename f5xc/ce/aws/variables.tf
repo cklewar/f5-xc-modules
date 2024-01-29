4b164c46278880d8ade09b65574309092f887cc4
c.klewar <c.klewar@f5.com>
2024-01-29 14:52:27 +0100

    update ami

--- f5xc/ce/aws/variables.tf
+++ f5xc/ce/aws/variables.tf
@@ -294,30 +294,6 @@ variable "f5xc_ce_machine_image" {
       us-west-1      = string
       us-west-2      = string
     })
-    voltstack = object({
-      af-south-1     = string
-      ap-east-1      = string
-      ap-northeast-1 = string
-      ap-northeast-2 = string
-      ap-northeast-3 = string
-      ap-south-1     = string
-      ap-southeast-1 = string
-      ap-southeast-2 = string
-      ap-southeast-3 = string
-      ca-central-1   = string
-      eu-central-1   = string
-      eu-north-1     = string
-      eu-south-1     = string
-      eu-west-1      = string
-      eu-west-2      = string
-      eu-west-3      = string
-      me-south-1     = string
-      sa-east-1      = string
-      us-east-1      = string
-      us-east-2      = string
-      us-west-1      = string
-      us-west-2      = string
-    })
   })
   default = {
     ingress_gateway = {
@@ -367,30 +343,6 @@ variable "f5xc_ce_machine_image" {
       us-east-2      = "ami-01ba94b5a83adcb35"
       us-west-1      = "ami-092a2a07d2d3a445f"
       us-west-2      = "ami-07252e5ab4023b8cf"
-    }
-    voltstack = {
-      af-south-1     = "ami-055ba977ad1ac6c6c"
-      ap-east-1      = "ami-05673740d6f3baee9"
-      ap-northeast-1 = "ami-030863f8dfd7029f5"
-      ap-northeast-2 = "ami-001dd539455cd4038"
-      ap-northeast-3 = ""
-      ap-south-1     = "ami-00788bd38d0fa4ff0"
-      ap-southeast-1 = "ami-0615e371749491e5f"
-      ap-southeast-2 = "ami-0538af7edde340eb1"
-      ap-southeast-3 = "ami-0f0c6b2822abb73e2"
-      ca-central-1   = "ami-0e1d39ac2c1c6ef2b"
-      eu-central-1   = "ami-094c24e0ff9141647"
-      eu-north-1     = "ami-0e939f8711e36b456"
-      eu-south-1     = "ami-0648b746bb1341bf4"
-      eu-west-1      = "ami-01ef385d886b812d2"
-      eu-west-2      = "ami-041138a60e1cb4314"
-      eu-west-3      = "ami-0e576d6275f207196"
-      me-south-1     = "ami-06603c1772bd574c2"
-      sa-east-1      = "ami-082f0a654c0936aa5"
-      us-east-1      = "ami-0f0926d6b6838b9cb"
-      us-east-2      = "ami-0d011fcc6cae3ed0a"
-      us-west-1      = "ami-0bec6c226bff67de2"
-      us-west-2      = "ami-0d2f1966d883656cd"
     }
   }
 }

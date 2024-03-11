749ada2f5867d1972b47ffc7c752cb81a6972812
c.klewar <c.klewar@f5.com>
2024-03-11 14:10:09 +0100

    rename cert

--- utils/url/outputs.tf
+++ utils/url/outputs.tf
@@ -4,7 +4,7 @@ output "data" {
     base        = local.base
     tenant      = local.tenant
     api_url     = local.api_url
-    p12_file    = local.cert
+    p12_file    = local.p12_file
     api_token   = local.api_token
     environment = local.environment
   }

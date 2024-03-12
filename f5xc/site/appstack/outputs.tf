0eac0d1f04abe3a3e0d1eb4a0417108d20757a7b
c.klewar <c.klewar@f5.com>
2024-03-12 16:19:56 +0100

    add eut appstack v37

--- f5xc/site/appstack/outputs.tf
+++ f5xc/site/appstack/outputs.tf
@@ -2,21 +2,8 @@ output "eut" {
   value = {
     type = "appstack"
     data = {
-      tenant = var.f5xc_tenant
-      # kubeconfig = module.kubeconfig_testbed.config
+      tenant     = var.f5xc_tenant
+      kubeconfig = module.kubeconfig_testbed.config
     }
   }
-}
-
-output "path" {
-  value = {
-    module   = path.module
-    cwd      = path.cwd
-    root     = path.root
-    absolute = abspath(path.module)
-  }
-}
-
-output "k8s_config_infra" {
-  value = module.kubeconfig_infrastructure
 }

4b772b26e1821dc553981e4a2b53786e6630cc69
c.klewar <c.klewar@f5.com>
2024-05-22 20:24:01 +0200

    update ci file

--- utils/kubeconfig/main.tf
+++ utils/kubeconfig/main.tf
@@ -1,11 +1,11 @@
 resource "time_offset" "exp_time" {
-  offset_days = 30
+  offset_days = var.offset_days
 }
 
 data "http" "kubeconfig" {
-  count           = fileexists(local.kubeconfig) ? 0 : 1
-  url             = format("%s/%s/%s/%s", var.f5xc_api_url, var.f5xc_rest_uri_sites, var.f5xc_k8s_cluster_name, var.f5xc_k8s_config_types[var.f5xc_k8s_config_type])
-  method          = "POST"
+  count  = fileexists(local.kubeconfig) ? 0 : 1
+  url    = format("%s/%s/%s/%s", var.f5xc_api_url, var.f5xc_rest_uri_sites, var.f5xc_k8s_cluster_name, var.f5xc_k8s_config_types[var.f5xc_k8s_config_type])
+  method = "POST"
   request_headers = {
     Authorization = format("APIToken %s", var.f5xc_api_token)
   }

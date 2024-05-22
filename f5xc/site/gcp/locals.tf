d1edf4532356dbc342d252ae360df002b258e3b3
c.klewar <c.klewar@f5.com>
2024-05-22 15:41:24 +0200

    add gcp eut v5

--- f5xc/site/gcp/locals.tf
+++ f5xc/site/gcp/locals.tf
@@ -1,6 +1,6 @@
 locals {
   f5xc_instance_names            = [for name in split(",", regex(local.f5xc_gcp_instance_name_pattern, volterra_tf_params_action.gcp_vpc_action.tf_output)[0]) : trim(name, "\"")]
-  f5xc_gcp_instance_name_pattern = "instance_names\\s*=\\s*\\[\"(.*?)\"\\]"
   f5xc_aws_ce_os_version         = var.f5xc_gcp_ce_os_version != "" && var.f5xc_gcp_default_ce_os_version == false ? var.f5xc_gcp_ce_os_version : null
   f5xc_aws_ce_sw_version         = var.f5xc_gcp_ce_sw_version != "" && var.f5xc_gcp_default_ce_sw_version == false ? var.f5xc_gcp_ce_sw_version : null
+  f5xc_gcp_instance_name_pattern = "instance_names\\s*=\\s*\\[\"(.*?)\"\\]"
 }
--- f5xc/site/gcp/output.tf
+++ f5xc/site/gcp/output.tf
@@ -3,8 +3,8 @@ output "gcp_vpc" {
     vpc   = volterra_gcp_vpc_site.site
     nodes = {
       node0 = data.google_compute_instance.node0
-      node1 = var.f5xc_gcp_node_number > 1 ? data.google_compute_instance.node1 : null
-      node2 = var.f5xc_gcp_node_number > 1 ? data.google_compute_instance.node2 : null
+      node1 = var.f5xc_gcp_node_number == 3 ? data.google_compute_instance.node1 : null
+      node2 = var.f5xc_gcp_node_number == 3 ? data.google_compute_instance.node2 : null
     }
     params = volterra_tf_params_action.gcp_vpc_action
   }

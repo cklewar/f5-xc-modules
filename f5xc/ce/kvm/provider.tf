d7641ebd94e2691408b590cb18489e84c57b20b1
ubuntu <wsronek@gmail.com>
2024-05-31 14:40:58 +0000

    kvm module fixes

--- f5xc/ce/kvm/provider.tf
+++ f5xc/ce/kvm/provider.tf
@@ -1,8 +1,8 @@
 provider "volterra" {
-  api_p12_file = "performance.staging.api-creds.p12"
-  url          = "https://performance.staging.volterra.us/api"
+  api_p12_file = var.f5xc_api_p12_file
+  url          = var.f5xc_api_url
 }
 
 provider "libvirt" {
-  uri   = "qemu:///system"
+  uri = var.qemu_uri
 }

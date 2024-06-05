ab3a98e5392427a3f699049e17214a42efd10e57
c.klewar <c.klewar@f5.com>
2024-06-06 00:43:43 +0200

    fix missing input v7

--- aws/ec2/data.tf
+++ aws/ec2/data.tf
@@ -1,3 +1,19 @@
+/*data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}*/
+
 data "aws_network_interface" "outside" {
   depends_on = [aws_instance.instance]
   id         = tolist(aws_instance.instance.network_interface)[0].network_interface_id

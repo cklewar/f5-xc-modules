ff8863be4422360a214c8ea90dae03b3c26b0136
c.klewar <c.klewar@f5.com>
2024-06-06 00:24:15 +0200

    fix missing input v5

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -1,25 +1,33 @@
 resource "aws_key_pair" "aws_key" {
-  key_name   = format("%s-key", var.aws_ec2_instance_name)
+  key_name = format("%s-key", var.aws_ec2_instance_name)
   public_key = var.ssh_public_key
 }
 
 module "network_interfaces" {
   source                        = "../network_interface"
-  count                         = length(var.aws_ec2_network_interfaces)
+  count = length(var.aws_ec2_network_interfaces)
   aws_interface_subnet_id       = var.aws_ec2_network_interfaces[count.index].subnet_id
   aws_interface_create_eip      = var.aws_ec2_network_interfaces[count.index].create_eip
   aws_interface_private_ips     = var.aws_ec2_network_interfaces[count.index].private_ips
   aws_interface_security_groups = var.aws_ec2_network_interfaces[count.index].security_groups
 }
 
+data "cloudinit_config" "config" {
+  gzip          = true
+  base64_encode = true
+  part {
+    content_type = "text/cloud-config"
+    content      = local.cloud_init_content
+  }
+}
+
 resource "aws_instance" "instance" {
-  ami                         = lookup(var.amis, var.aws_region)
-  tags                        = merge({ "Name" : var.aws_ec2_instance_name, "Owner" : var.owner }, var.custom_tags)
+  ami = lookup(var.amis, var.aws_region)
+  tags = merge({ "Name" : var.aws_ec2_instance_name, "Owner" : var.owner }, var.custom_tags)
   key_name                    = aws_key_pair.aws_key.key_name
-  user_data                   = local.cloud_init_content
+  user_data                   = data.cloudinit_config.config.rendered
   instance_type               = var.aws_ec2_instance_type
   user_data_replace_on_change = var.aws_ec2_user_data_replace_on_change
-
 
   dynamic "network_interface" {
     for_each = var.aws_ec2_network_interfaces_ref
@@ -40,14 +48,14 @@ resource "aws_instance" "instance" {
 
 resource "local_file" "instance_script" {
   depends_on = [aws_instance.instance]
-  count      = var.aws_ec2_instance_script_template != "" ? 1 : 0
-  content    = local.script_content
-  filename   = "${var.template_output_dir_path}/${var.aws_ec2_instance_script_file}"
+  count    = var.aws_ec2_instance_script_template != "" ? 1 : 0
+  content  = local.script_content
+  filename = "${var.template_output_dir_path}/${var.aws_ec2_instance_script_file}"
 }
 
 resource "terraform_data" "ec2_instance_provision_custom_data" {
   depends_on = [aws_instance.instance, local_file.instance_script]
-  for_each   = {for item in var.aws_ec2_instance_custom_data_dirs : item.name => item if var.aws_ec2_instance_script_template != ""}
+  for_each = {for item in var.aws_ec2_instance_custom_data_dirs : item.name => item if var.aws_ec2_instance_script_template != ""}
 
   connection {
     type        = var.provisioner_connection_type
@@ -63,7 +71,7 @@ resource "terraform_data" "ec2_instance_provision_custom_data" {
 }
 
 resource "terraform_data" "ec2_execute_script_file" {
-  count      = var.aws_ec2_instance_script_template != "" ? 1 : 0
+  count = var.aws_ec2_instance_script_template != "" ? 1 : 0
   depends_on = [terraform_data.ec2_instance_provision_custom_data]
   connection {
     type        = var.provisioner_connection_type

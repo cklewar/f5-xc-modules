34de10b83d7596075871aad7a903716100491bb1
c.klewar <c.klewar@f5.com>
2024-06-07 22:10:45 +0200

    add azure v22

--- azure/linux_virtual_machine/main.tf
+++ azure/linux_virtual_machine/main.tf
@@ -20,6 +20,7 @@ resource "azurerm_network_interface" "network_interface" {
     name = format("%s-ip-cfg", var.azure_network_interfaces[count.index].name)
     subnet_id = var.azure_network_interfaces[count.index].ip_configuration.subnet_id
     # private_ip_address            = var.azure_network_interfaces[count.index].ip_configuration.private_ip_address_allocation == "Static" ? var.azure_network_interfaces[count.index].ip_configuration.private_ip_address : null
+    private_ip_address            = "192.168.168.7"
     public_ip_address_id          = contains(keys(azurerm_public_ip.ip), var.azure_network_interfaces[count.index].name) ? azurerm_public_ip.ip[var.azure_network_interfaces[count.index].name].id : null
     private_ip_address_allocation = var.azure_network_interfaces[count.index].ip_configuration.private_ip_address_allocation
   }

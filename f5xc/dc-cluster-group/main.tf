aa92a5210769269b202781f3c68c4a67bda15f39
c.klewar <c.klewar@f5.com>
2024-09-23 12:49:53 +0200

    add ce_performance_l3 v11

--- f5xc/dc-cluster-group/main.tf
+++ f5xc/dc-cluster-group/main.tf
@@ -1,6 +1,6 @@
 resource "volterra_dc_cluster_group" "dc_cluster_group" {
   name        = var.f5xc_dc_cluster_group_name
-  namespace   = var.f5xc_namespace
   labels      = var.f5xc_dc_cluster_group_labels
+  namespace   = var.f5xc_namespace
   description = var.f5xc_dc_cluster_group_description != "" ? var.f5xc_dc_cluster_group_description : null
 }

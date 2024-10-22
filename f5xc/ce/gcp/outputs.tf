df83eec79aa1f90e3e0263afa296aa65d0a00189
c.klewar <c.klewar@f5.com>
2024-10-22 17:28:10 +0200

    0.11.37 v13

--- f5xc/ce/gcp/outputs.tf
+++ f5xc/ce/gcp/outputs.tf
@@ -1,8 +1,8 @@
 output "ce" {
   value = {
-    nodes                   = module.node.ce
-    network                 = module.network_common
-    firewall                = local.create_network ? module.firewall.ce : null
-    secure_mesh             = module.secure_mesh_site
+    nodes            = module.node.ce
+    network          = module.network_common
+    firewall         = local.create_network ? module.firewall.ce : null
+    secure_mesh_site = var.f5xc_secure_mesh_site_version == 1 ? module.secure_mesh_site.0.secure_mesh_site : module.secure_mesh_site_v2.0.secure_mesh_site
   }
 }
